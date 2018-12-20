0ab4d3d892f354d8d8ab528dc01b1f25ed90293b
David Wu <lightvector@gmail.com>
2018-12-20 04:34:09 +0000

    Reduce gatekeeper delay checking for new nets

--- cpp/gatekeeper.cpp
+++ cpp/gatekeeper.cpp
@@ -423,8 +423,8 @@ int MainCmds::gatekeeper(int argc, const char* const* argv) {
     netAndStuff = loadLatestNeuralNet();
 
     if(netAndStuff == NULL) {
-      for(int i = 0; i<12; i++) {
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+      for(int i = 0; i<5; i++) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
         if(shouldStop.load())
           break;
       }
