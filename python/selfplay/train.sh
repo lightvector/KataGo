b4a4285fba4092ba6863a958876fee17f7dff5a9
David Wu <lightvector@gmail.com>
2019-02-02 05:48:54 +0000

    Revert writing models twice as often

--- python/selfplay/train.sh
+++ python/selfplay/train.sh
@@ -34,10 +34,10 @@ time python3 ./train.py \
      -exportprefix $TRAININGNAME \
      -pos-len 19 \
      -batch-size 256 \
-     -samples-per-epoch 500000 \
+     -samples-per-epoch 1000000 \
      -gpu-memory-frac 0.6 \
      -model-kind $MODELKIND \
-     -sub-epochs 2 \
+     -sub-epochs 4 \
      -swa-sub-epoch-scale 4 \
      "$@" \
      2>&1 | tee -a $BASEDIR/train/$TRAININGNAME/stdout.txt
