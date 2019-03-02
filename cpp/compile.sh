5956e409fb6b680d17774cc03ff2bc1576056941
David Wu <lightvector@gmail.com>
2019-03-02 16:09:32 +0000

    Add deprecation message to super-old compile script

--- cpp/compile.sh
+++ cpp/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 
+echo "This script is deprecated, use cmake instead"
+
 /usr/bin/h5c++ -std=c++14 -fmessage-length=0 -Wall -Wno-sign-compare -Wno-strict-aliasing -Itclap-1.2.1/include -O3 write.cpp game/*.cpp dataio/*.cpp core/*.cpp -o write.exe
 
 echo "Done"
