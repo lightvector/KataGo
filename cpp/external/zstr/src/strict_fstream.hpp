8e8609210503a4fc309b02a1455d33de77c27367
Loren Puchalla Fiore <loren.fiore@gmail.com>
2019-04-28 12:24:39 -0400

    Update strict_fstream.hpp strerror_r logic.

--- cpp/external/zstr/src/strict_fstream.hpp
+++ cpp/external/zstr/src/strict_fstream.hpp
@@ -27,7 +27,7 @@ static std::string strerror()
     {
         buff = "Unknown error";
     }
-#elif defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+#elif (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || (defined (__APPLE__) && defined (__MACH__))) && ! _GNU_SOURCE
 // XSI-compliant strerror_r()
     if (strerror_r(errno, &buff[0], buff.size()) != 0)
     {
