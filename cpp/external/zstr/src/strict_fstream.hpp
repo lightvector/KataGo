a3dbbff38cd8330f7d2cf8480a70d0d8cded0df8
David Wu <lightvector@gmail.com>
2019-07-06 22:30:55 +0000

    Fix catch by value exn warning in zstr

--- cpp/external/zstr/src/strict_fstream.hpp
+++ cpp/external/zstr/src/strict_fstream.hpp
@@ -125,7 +125,7 @@ struct static_method_holder
             is_p->peek();
             peek_failed = is_p->fail();
         }
-        catch (std::ios_base::failure e) {}
+        catch (std::ios_base::failure& e) {}
         if (peek_failed)
         {
             throw Exception(std::string("strict_fstream: open('")
