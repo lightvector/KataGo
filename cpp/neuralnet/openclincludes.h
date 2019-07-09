966869d3fc4b86d643cfc5e105e94290c589669d
David Wu <lightvector@gmail.com>
2019-07-09 05:45:55 +0000

    Target opencl version 1.2

--- cpp/neuralnet/openclincludes.h
+++ cpp/neuralnet/openclincludes.h
@@ -2,6 +2,7 @@
 #define NEURALNET_OPENCLINCLUDES_H
 
 //Ensures a consistent opencl version everywhere we include opencl
+#define CL_TARGET_OPENCL_VERSION 120
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 
 #ifdef __APPLE__
