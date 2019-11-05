ea51493e968951b777023d6aedfb5b1ab4d86312
David Wu <lightvector@gmail.com>
2019-11-05 03:50:41 +0000

    Enable japanese rules in python model configs

--- python/modelconfigs.py
+++ python/modelconfigs.py
@@ -16,7 +16,7 @@ from model import Model
 
 b6c96 = {
   "version":7,
-  "support_japanese_rules":False,
+  "support_japanese_rules":True,
   "trunk_num_channels":96,
   "mid_num_channels":96,
   "regular_num_channels":64,
@@ -39,7 +39,7 @@ b6c96 = {
 
 b10c128 = {
   "version":7,
-  "support_japanese_rules":False,
+  "support_japanese_rules":True,
   "trunk_num_channels":128,
   "mid_num_channels":128,
   "regular_num_channels":96,
@@ -66,7 +66,7 @@ b10c128 = {
 
 b15c192 = {
   "version":7,
-  "support_japanese_rules":False,
+  "support_japanese_rules":True,
   "trunk_num_channels":192,
   "mid_num_channels":192,
   "regular_num_channels":128,
@@ -98,7 +98,7 @@ b15c192 = {
 
 b20c256 = {
   "version":7,
-  "support_japanese_rules":False,
+  "support_japanese_rules":True,
   "trunk_num_channels":256,
   "mid_num_channels":256,
   "regular_num_channels":192,
