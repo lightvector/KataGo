38c7e770ccf629635267304ae1bbc6bf5fa54516
David Wu <lightvector@gmail.com>
2019-11-23 16:01:45 +0000

    One more fix

--- python/model.py
+++ python/model.py
@@ -55,7 +55,7 @@ class Model:
       assert(False)
 
 
-  def __init__(self,config,pos_len,placeholders,is_training_bool):
+  def __init__(self,config,pos_len,placeholders,is_training=False):
     self.pos_len = pos_len
     self.num_bin_input_features = Model.get_num_bin_input_features(config)
     self.num_global_input_features = Model.get_num_global_input_features(config)
@@ -89,8 +89,8 @@ class Model:
     self.reg_variables = []
     self.prescale_variables = []
     self.lr_adjusted_variables = {}
-    self.is_training = (placeholders["is_training"] if "is_training" in placeholders else tf.placeholder(tf.bool,name="is_training"))
-    self.is_training_bool = is_training_bool
+    self.is_training = is_training
+    self.is_training_tensor = tf.constant(is_training,dtype=tf.bool)
 
     #Accumulates outputs for printing stats about their activations
     self.outputs_by_layer = []
@@ -520,7 +520,7 @@ class Model:
     def inference_f():
       return (moving_mean,moving_var)
 
-    use_mean,use_var = tf.cond(self.is_training,training_f,inference_f)
+    use_mean,use_var = tf.cond(self.is_training_tensor,training_f,inference_f)
     return tf.nn.batch_normalization(tensor,use_mean,use_var,beta,None,epsilon) * mask
 
   # def batchnorm(self,name,tensor):
@@ -535,7 +535,7 @@ class Model:
   #     epsilon=epsilon,
   #     center=has_bias,
   #     scale=has_scale,
-  #     training=self.is_training,
+  #     training=self.is_training_tensor,
   #     name=name,
   #   )
 
@@ -1411,7 +1411,7 @@ class Target_vars:
       / (1.0 + tf.reduce_sum(tf.reshape(model.mask_before_symmetry,[-1,model.pos_len,model.pos_len]),axis=[1,2]))
     )
     unowned_proportion = tf.reduce_mean(unowned_proportion * self.ownership_target_weight)
-    if model.is_training_bool:
+    if model.is_training:
       moving_unowned_proportion = tf.Variable(1.0,name=("moving_unowned_proportion"),trainable=False)
       moving_unowned_op = tf.keras.backend.moving_average_update(moving_unowned_proportion,unowned_proportion,0.998)
       with tf.control_dependencies([moving_unowned_op]):
@@ -1612,8 +1612,7 @@ class ModelUtils:
     placeholders["symmetries"] = tf.greater(tf.random_uniform([3],minval=0,maxval=2,dtype=tf.int32),tf.zeros([3],dtype=tf.int32))
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-      placeholders["is_training"] = tf.constant(False,dtype=tf.bool)
-      model = Model(model_config,pos_len,placeholders,is_training_bool=False)
+      model = Model(model_config,pos_len,placeholders,is_training=False)
       return model
 
     placeholders["include_history"] = features["gtnc"][:,36:41]
@@ -1645,16 +1644,14 @@ class ModelUtils:
     placeholders["l2_reg_coeff"] = tf.constant(l2_coeff_value,dtype=tf.float32)
 
     if mode == tf.estimator.ModeKeys.EVAL:
-      placeholders["is_training"] = tf.constant(False,dtype=tf.bool)
-      model = Model(model_config,pos_len,placeholders,is_training_bool=False)
+      model = Model(model_config,pos_len,placeholders,is_training=False)
 
       target_vars = Target_vars(model,for_optimization=True,placeholders=placeholders)
       metrics = Metrics(model,target_vars,include_debug_stats=False)
       return (model,target_vars,metrics)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
-      placeholders["is_training"] = tf.constant(True,dtype=tf.bool)
-      model = Model(model_config,pos_len,placeholders,is_training_bool=True)
+      model = Model(model_config,pos_len,placeholders,is_training=True)
 
       target_vars = Target_vars(model,for_optimization=True,placeholders=placeholders)
       metrics = Metrics(model,target_vars,include_debug_stats=False)
