d23b3a5e462f6a68014eb994b1ef50110ee2906b
David Wu <lightvector@gmail.com>
2019-11-30 06:29:22 +0000

    Update python to actually use version 8 models

--- python/modelconfigs.py
+++ python/modelconfigs.py
@@ -15,7 +15,7 @@ import numpy as np
 from model import Model
 
 b6c96 = {
-  "version":7,
+  "version":8,
   "support_japanese_rules":True,
   "trunk_num_channels":96,
   "mid_num_channels":96,
@@ -38,7 +38,7 @@ b6c96 = {
 }
 
 b10c128 = {
-  "version":7,
+  "version":8,
   "support_japanese_rules":True,
   "trunk_num_channels":128,
   "mid_num_channels":128,
@@ -65,7 +65,7 @@ b10c128 = {
 }
 
 b15c192 = {
-  "version":7,
+  "version":8,
   "support_japanese_rules":True,
   "trunk_num_channels":192,
   "mid_num_channels":192,
@@ -97,7 +97,7 @@ b15c192 = {
 }
 
 b20c256 = {
-  "version":7,
+  "version":8,
   "support_japanese_rules":True,
   "trunk_num_channels":256,
   "mid_num_channels":256,
