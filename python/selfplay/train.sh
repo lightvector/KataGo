dbd820feeae91299d20234d8e32b18c502dbf4bf
David Wu <lightvector@gmail.com>
2019-12-21 00:43:17 +0000

    Adjust export mode args for train script

--- python/selfplay/train.sh
+++ python/selfplay/train.sh
@@ -2,15 +2,17 @@
 
 #Runs tensorflow training in train/$TRAININGNAME
 #Should be run once per persistent training process.
-#Outputs results in tfsavedmodels_toexport/ in an ongoing basis.
+#Outputs results in tfsavedmodels_toexport/ in an ongoing basis (EXPORTMODE == "main").
+#Or, to tfsavedmodels_toexport_extra/ (EXPORTMODE == "extra").
+#Or just trains without exporting (EXPORTMODE == "trainonly").
 
 if [[ $# -lt 4 ]]
 then
-    echo "Usage: $0 BASEDIR TRAININGNAME MODELKIND MAINOEXTRA OTHERARGS"
+    echo "Usage: $0 BASEDIR TRAININGNAME MODELKIND EXPORTMODE OTHERARGS"
     echo "BASEDIR containing selfplay data and models and related directories"
     echo "TRANINGNAME name to prefix models with, specific to this training daemon"
     echo "MODELKIND what size model to train"
-    echo "MAINOREXTRA if 'extra', outputs to tfsavedmodels_toexport_extra/ instead."
+    echo "EXPORTMODE 'main': train and export for selfplay. 'extra': train and export extra non-selfplay model. 'trainonly': train without export"
     exit 0
 fi
 BASEDIR="$1"
@@ -19,7 +21,7 @@ TRAININGNAME="$1"
 shift
 MODELKIND="$1"
 shift
-MAINOREXTRA="$1"
+EXPORTMODE="$1"
 shift
 
 #------------------------------------------------------------------------------
@@ -30,11 +32,19 @@ git show --no-patch --no-color > "$BASEDIR"/train/"$TRAININGNAME"/version.txt
 git diff --no-color > "$BASEDIR"/train/"$TRAININGNAME"/diff.txt
 git diff --staged --no-color > "$BASEDIR"/train/"$TRAININGNAME"/diffstaged.txt
 
-if [ "$MAINOREXTRA" == "extra" ]
+if [ "$EXPORTMODE" == "main" ]
 then
-    EXPORT_SUBDIR=tfsavedmodels_toexport_extra
-else
     EXPORT_SUBDIR=tfsavedmodels_toexport
+    EXTRAFLAG=""
+elif [ "$EXPORTMODE" == "extra" ]
+    EXPORT_SUBDIR=tfsavedmodels_toexport_extra
+    EXTRAFLAG=""
+elif [ "$EXPORTMODE" == "trainonly" ]
+    EXPORT_SUBDIR=tfsavedmodels_toexport_extra
+    EXTRAFLAG="-no-export"
+else
+    echo "EXPORTMODE was not 'main' or 'extra' or 'trainonly', run with no arguments for usage"
+    exit 1
 fi
 
 time python3 ./train.py \
@@ -49,5 +59,6 @@ time python3 ./train.py \
      -model-kind "$MODELKIND" \
      -sub-epochs 4 \
      -swa-sub-epoch-scale 4 \
+     $EXTRAFLAG \
      "$@" \
      2>&1 | tee -a "$BASEDIR"/train/"$TRAININGNAME"/stdout.txt
