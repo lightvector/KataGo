f288389abea6da45a47f5b2b3252ffe462b91d6a
David Wu <lightvector@gmail.com>
2020-01-18 04:38:04 +0000

    Bump version to 1.3.1

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -159,11 +159,11 @@ int main(int argc, const char* argv[]) {
 
 
 string Version::getKataGoVersion() {
-  return string("1.3");
+  return string("1.3.1");
 }
 
 string Version::getKataGoVersionForHelp() {
-  return string("KataGo v1.3");
+  return string("KataGo v1.3.1");
 }
 
 string Version::getGitRevision() {
