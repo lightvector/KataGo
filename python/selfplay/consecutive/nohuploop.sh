feceb32427086d0b540f031dd0159010d3375367
iopq <iop_jr@yahoo.com>
2020-03-20 00:18:04 +0800

    saving PID in a file for easy killing

--- python/selfplay/consecutive/nohuploop.sh
+++ python/selfplay/consecutive/nohuploop.sh
@@ -7,3 +7,6 @@ TRAININGNAME="freshtwenty"
 MODELKIND="b20c256"
 
 nohup ./blockingloop.sh "$BASEDIR" "$SCRIPTDIR" "$KATAEXEC" "$THREADS" "$TRAININGNAME" "$MODELKIND" >> log_all.txt 2>&1 &
+echo $! > save_pid.txt
+#if you want to stop the loop without rebooting the machine run the following command
+#kill `cat save_pid.txt`
