624aa219deabcf8ece3b70b2ef525356855b71c8
iopq <iop_jr@yahoo.com>
2020-03-20 16:35:33 +0800

    I'll call out to this script now
    I'll think about how to improve it later, but let's do this first

---
+++ python/selfplay/consecutive/trainonce.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eu
+
+if [[ $# -lt 5 ]]
+then
+    echo "Usage: $0 BASEDIR SCRIPTDIR TRAININGNAME MODELKIND EXPORTMODE OTHERARGS"
+    echo "BASEDIR containing selfplay data and models and related directories"
+    echo "SCRIPTDIR containing the KataGo python folder"
+    echo "TRANINGNAME name to prefix models with, specific to this training daemon"
+    echo "MODELKIND what size model to train"
+    echo "EXPORTMODE 'main': train and export for selfplay. 'extra': train and export extra non-selfplay model. 'trainonly': train without export"
+    exit 0
+fi
+
+BASEDIR="$1"
+shift
+SCRIPTDIR="$1"
+shift
+TRAININGNAME="$1"
+shift
+MODELKIND="$1"
+shift
+EXPORTMODE="$1"
+shift
+
+cd "$SCRIPTDIR" && ./selfplay/train.sh "$BASEDIR"/ "$TRAININGNAME" "$MODELKIND" "$TRAININGNAME" -max-epochs-this-instance 1 "$*"
