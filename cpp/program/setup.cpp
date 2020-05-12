44d582e078d7b04d7ba79d22d657f91b83b54b59
David Wu <lightvector@gmail.com>
2020-05-12 22:44:33 +0000

    Make pondering limits default to unlimited now instead of non-ponder limits

--- cpp/program/setup.cpp
+++ cpp/program/setup.cpp
@@ -312,13 +312,13 @@ vector<SearchParams> Setup::loadParams(
 
     if(cfg.contains("maxPlayoutsPondering"+idxStr)) params.maxPlayoutsPondering = cfg.getInt64("maxPlayoutsPondering"+idxStr, (int64_t)1, (int64_t)1 << 50);
     else if(cfg.contains("maxPlayoutsPondering"))   params.maxPlayoutsPondering = cfg.getInt64("maxPlayoutsPondering",        (int64_t)1, (int64_t)1 << 50);
-    else                                            params.maxPlayoutsPondering = params.maxPlayouts;
+    else                                            params.maxPlayoutsPondering = (int64_t)1 << 50;
     if(cfg.contains("maxVisitsPondering"+idxStr)) params.maxVisitsPondering = cfg.getInt64("maxVisitsPondering"+idxStr, (int64_t)1, (int64_t)1 << 50);
     else if(cfg.contains("maxVisitsPondering"))   params.maxVisitsPondering = cfg.getInt64("maxVisitsPondering",        (int64_t)1, (int64_t)1 << 50);
-    else                                          params.maxVisitsPondering = params.maxVisits;
+    else                                          params.maxVisitsPondering = (int64_t)1 << 50;
     if(cfg.contains("maxTimePondering"+idxStr)) params.maxTimePondering = cfg.getDouble("maxTimePondering"+idxStr, 0.0, 1.0e20);
     else if(cfg.contains("maxTimePondering"))   params.maxTimePondering = cfg.getDouble("maxTimePondering",        0.0, 1.0e20);
-    else                                        params.maxTimePondering = params.maxTime;
+    else                                        params.maxTimePondering = 1.0e20;
 
     if(cfg.contains("lagBuffer"+idxStr)) params.lagBuffer = cfg.getDouble("lagBuffer"+idxStr, 0.0, 3600.0);
     else if(cfg.contains("lagBuffer"))   params.lagBuffer = cfg.getDouble("lagBuffer",        0.0, 3600.0);
