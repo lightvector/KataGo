8ea68f93b2361a4d4a177c57ed1831cecd33e6fa
David Wu <lightvector@gmail.com>
2020-06-01 19:31:22 +0000

    Update upload loop and add looping backoff for errors

--- python/selfplay/upload_model_for_selfplay.sh
+++ python/selfplay/upload_model_for_selfplay.sh
@@ -4,17 +4,20 @@ set -o pipefail
 #Takes any models in modelstobetested/ and uploads them, then moves them to modelsuploaded/
 #Should be run periodically.
 
-if [[ $# -ne 2 ]]
+if [[ $# -ne 3 ]]
 then
-    echo "Usage: $0 NAMEPREFIX BASEDIR"
+    echo "Usage: $0 RUNNAME BASEDIR CONNECTION_CONFIG"
     echo "Currently expects to be run from within the 'python' directory of the KataGo repo, or otherwise in the same dir as export_model.py."
-    echo "NAMEPREFIX string prefix for this training run, try to pick something globally unique. Will be displayed to users when KataGo loads the model."
+    echo "RUNNAME should match what the server uses as the run name, try to pick something globally unique. Will prefix model names in uploaded files."
     echo "BASEDIR containing selfplay data and models and related directories"
+    echo "CONNECTION_CONFIG config containing serverUrl, username, password"
     exit 0
 fi
-NAMEPREFIX="$1"
+RUNNAME="$1"
 shift
 BASEDIR="$1"
+shift
+CONNECTION_CONFIG="$1"
 shift
 
 #------------------------------------------------------------------------------
@@ -47,14 +50,14 @@ function uploadStuff() {
                 rm -rf "$TMPDST"
                 mkdir "$TMPDST"
 
-                cp "$SRC"/model.bin "$TMPDST"/"NAMEPREFIX"-"$NAME".bin
-                gzip "$TMPDST"/"NAMEPREFIX"-"$NAME".bin
+                cp "$SRC"/model.bin "$TMPDST"/"RUNNAME"-"$NAME".bin
+                gzip "$TMPDST"/"RUNNAME"-"$NAME".bin
                 cp "$SRC"/model.config.json "$TMPDST"/model.config.json
                 cp -r "$SRC"/saved_model "$TMPDST"/saved_model
-                zip -r "$TMPDST"/"NAMEPREFIX"-"$NAME".zip "$TMPDST"/"NAMEPREFIX"-"$NAME".bin.gz "$TMPDST"/model.config.json "$TMPDST"/saved_model
-                rm -r "$TMPDST"/"NAMEPREFIX"-"$NAME".bin.gz "$TMPDST"/model.config.json "$TMPDST"/saved_model
+                zip -r "$TMPDST"/"RUNNAME"-"$NAME".zip "$TMPDST"/"RUNNAME"-"$NAME".bin.gz "$TMPDST"/model.config.json "$TMPDST"/saved_model
+                rm -r "$TMPDST"/"RUNNAME"-"$NAME".bin.gz "$TMPDST"/model.config.json "$TMPDST"/saved_model
                 cp -r "$SRC"/non_swa_saved_model "$TMPDST"/non_swa_saved_model
-                zip -r "$TMPDST"/"NAMEPREFIX"-"$NAME"_non_swa.zip "$TMPDST"/non_swa_saved_model
+                zip -r "$TMPDST"/"RUNNAME"-"$NAME"_non_swa.zip "$TMPDST"/non_swa_saved_model
                 rm -r "$TMPDST"/non_swa_saved_model
                 cp -r "$SRC"/trainhistory.json "$TMPDST"/trainhistory.json
                 cp -r "$SRC"/log.txt "$TMPDST"/log.txt
@@ -62,16 +65,34 @@ function uploadStuff() {
                 #Sleep a little to allow some tolerance on the filesystem
                 sleep 3
 
-                set -x
-                python3 ./upload_model.py \
-                        -run-name "$NAMEPREFIX" \
-                        -model-name "$NAME" \
-                        -model-file "$TMPDST"/"NAMEPREFIX"-"$NAME".bin.gz \
-                        -model-zip "$TMPDST"/"NAMEPREFIX"-"$NAME".zip \
-                        -upload-log-file "$TMPDST"/upload_log.txt \
-                        -uploaded_dir "$TARGETDIR" \
-                        -base-server-url localhost
-                set +x
+                SUCCESSFUL=0
+                BACKOFF=10
+                while [ $SUCCESSFUL -ne 1 ]
+                do
+                    set -x
+                    python3 ./upload_model.py \
+                            -run-name "$RUNNAME" \
+                            -model-name "$RUNNAME"-"$NAME" \
+                            -model-file "$TMPDST"/"RUNNAME"-"$NAME".bin.gz \
+                            -model-zip "$TMPDST"/"RUNNAME"-"$NAME".zip \
+                            -upload-log-file "$TMPDST"/upload_log.txt \
+                            -parents-dir "$TARGETDIR" \
+                            -connection-config "$CONNECTION_CONFIG"
+                    RESULT=$?
+                    set +x
+                    if [ $RESULT -ne 0 ]
+                    then
+                        echo "Sleeping $BACKOFF seconds before trying again"
+                        sleep "$BACKOFF"
+                        BACKOFF=$(( BACKOFF * 4 / 3 ))
+                        if [ $BACKOFF -gt 10000 ]
+                        then
+                            BACKOFF=10000
+                        fi
+                    else
+                        SUCCESSFUL=1
+                    fi
+                done
 
                 #Sleep a little to allow some tolerance on the filesystem
                 sleep 3
