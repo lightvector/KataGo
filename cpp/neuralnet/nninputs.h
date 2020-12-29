896df60a01d11933d8c31b8b6a74a8635490d173
Jonathan Roy <jonroy7@gmail.com>
2020-12-29 14:40:41 -0500

    Fix typo

--- cpp/neuralnet/nninputs.h
+++ cpp/neuralnet/nninputs.h
@@ -32,7 +32,7 @@ struct MiscNNInputParams {
   float nnPolicyTemperature = 1.0f;
   bool avoidMYTDaggerHack = false;
   // Used to pass the requested symmetry down into getOutput(). -1 means no specific symmetry requested,
-  // therefor use default or random based on config.
+  // therefore use default or random based on config.
   int symmetry = -1;
 
   static const Hash128 ZOBRIST_CONSERVATIVE_PASS;
