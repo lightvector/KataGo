e6fea1e71b2f74db29f9c87d1604078795a6b6a8
Jonathan Roy <jonroy7@gmail.com>
2020-12-29 14:18:54 -0500

    Add comment about NNResultBuf.symmetry

--- cpp/neuralnet/nneval.h
+++ cpp/neuralnet/nneval.h
@@ -55,7 +55,7 @@ struct NNResultBuf {
   float* rowGlobal;
   std::shared_ptr<NNOutput> result;
   bool errorLogLockout; //error flag to restrict log to 1 error to prevent spam
-  int symmetry;
+  int symmetry; //The symmetry to use
 
   NNResultBuf();
   ~NNResultBuf();
