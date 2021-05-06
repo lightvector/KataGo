c0d1489831f525899157f78583c6f037171ed445
David Wu <lightvector@gmail.com>
2021-05-06 01:34:12 +0000

    Add some direct score table tests

--- cpp/tests/testscore.cpp
+++ cpp/tests/testscore.cpp
@@ -141,4 +141,28 @@ xxxxx
     cout << endl;
   }
 
+  {
+    const char* name = "Score value tables";
+    cout << name << endl;
+    for(int center = 0; center <= 5; center += 5) {
+      for(int scale = 1; scale <= 2; scale++) {
+        for(int b = 0; b<5; b++) {
+          int xSizes[5] = {9, 13, 13, 13, 19};
+          int ySizes[5] = {9, 9, 13, 19, 19};
+          Board board(xSizes[b],ySizes[b]);
+          cout << "center " << center << " scale " << scale << " x " << xSizes[b] << " y " << ySizes[b] << endl;
+          for(int stdev = 0; stdev <= 5; stdev++) {
+            for(double d = -8.0; d<=8.0; d += 0.5) {
+              double scoreValue = ScoreValue::expectedWhiteScoreValue(d, stdev, center, scale, board);
+              cout << Global::strprintf("%.3f ", scoreValue);
+            }
+            cout << endl;
+          }
+          cout << endl;
+        }
+        cout << endl;
+      }
+    }
+  }
+
 }
