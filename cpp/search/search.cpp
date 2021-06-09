1a6bba16a689eec914be31faa7665d97a32c5daf
TFiFiE <TFiFiE@users.noreply.github.com>
2021-06-09 03:10:38 +0200

    Eliminated some compiler warnings

--- cpp/search/search.cpp
+++ cpp/search/search.cpp
@@ -3287,7 +3287,7 @@ bool Search::playoutDescend(
       if(searchParams.subtreeValueBiasFactor != 0) {
         if(node.prevMoveLoc != Board::NULL_LOC) {
           assert(subtreeValueBiasTable != NULL);
-          child->subtreeValueBiasTableEntry = std::move(subtreeValueBiasTable->get(thread.pla, node.prevMoveLoc, child->prevMoveLoc, thread.board));
+          child->subtreeValueBiasTableEntry = subtreeValueBiasTable->get(thread.pla, node.prevMoveLoc, child->prevMoveLoc, thread.board);
         }
       }
 
