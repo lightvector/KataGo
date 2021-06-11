e19960cf34fa399a690f2019594d3547c3e5bcb1
fuhaoda <fu_haoda@lilly.com>
2021-06-11 11:34:16 -0400

    updated codes and remove self._new_sgf_files and self._new_sgfs_files and also allow foo/bar/*.sgfs as input dir

--- python/summarize_sgfs.py
+++ python/summarize_sgfs.py
@@ -1,3 +1,4 @@
+import glob
 import itertools
 
 from sgfmill import sgf
@@ -38,11 +39,11 @@ class GameResultSummary:
 
     call it by other function:
     import summarize_sgfs
-    elo_prior = 3400
-    gameResultSummary = summarize_sgfs.GameResultSummary(elo_prior)
-    gameResultSummary.add_games(input_file_dir)
-    gameResultSummary.print_game_results()
-    gameResultSummary.print_elos()
+    elo_prior = 50
+    game_result_summary = summarize_sgfs.GameResultSummary(elo_prior)
+    game_result_summary.add_games(input_file_dir)
+    game_result_summary.print_game_results()
+    game_result_summary.print_elos()
   """
 
   def __init__(
@@ -53,38 +54,34 @@ class GameResultSummary:
 
     self._all_sgfs_files = set()
     self._all_sgf_files = set()
-    self._new_sgfs_files = set()
-    self._new_sgf_files = set()
-    self._warning = False
-    self._elo_prior = elo_prior
+    self._should_warn_handicap_komi = False
+    self._elo_prior = elo_prior #number of prior games to stablize the results.
 
   """ Public functions """
 
   def add_games(self, input_file_dir, search_subdir=False):
     """add files under input_file_dir into the results and duplicate files will not be added."""
-    self._add_game_file_names(input_file_dir, search_subdir)
-    self._add_new_games_to_result_dict()
+    new_files = self._add_game_file_names(input_file_dir, search_subdir)
+    self._add_new_games_to_result_dict(new_files)
 
   def add_a_game_file(self, input_file_name):
     """add a new game file (sgfs or sgf) into the results and duplicate file will not be added."""
-    self._add_a_game_file_name(input_file_name)
-    self._add_new_games_to_result_dict()
+    new_file = self._add_a_game_file_name(input_file_name)
+    self._add_new_games_to_result_dict(new_file)
 
   def clear(self):
     """Clear all the records and reset to empty sets and empty dictionaries"""
     self.results = {}  # a dictionary as {(black_player_name, white_player_name):Record}
     self._all_sgfs_files = set()
     self._all_sgf_files = set()
-    self._new_sgfs_files = set()
-    self._new_sgf_files = set()
-    self._warning = False
+    self._should_warn_handicap_komi = False
 
   def print_elos(self) -> elo.EloInfo:
     """Print estimated Bayes Elo scores centered as Elo prior"""
     elo_info = self._estimate_elo()
     print(elo_info)
     print(f"The prior is set as {self._elo_prior}, and the estimated Bayes Elo:")
-    if self._warning:
+    if self._should_warn_handicap_komi:
       warnings.warn("There are handicap games or games with komi < 5.5 or komi > 7.5")
     return elo_info
 
@@ -109,74 +106,89 @@ class GameResultSummary:
   """ Private functions starts here, and no more public functions below """
 
   def _add_game_file_names(self, input_file_dir, search_subdir):
+    # handle user specifying multiple sgf files to the script via glob on the terminal, for example foo/bar/*.sgf"
+    print(f"Searching and adding files in {input_file_dir}, please wait ...")
+    if input_file_dir.lower().endswith((".sgf", ".sgfs")):
+      files = glob.glob(input_file_dir)
+      for file in files:
+        self.add_a_game_file(file)
+      print(f"We found {len(files)} new sgfs/sgf files in after parse {input_file_dir}.")
+      return {"new_sgfs_files": [], "new_sgf_files": []}
+
     if not os.path.exists(input_file_dir):
       print(f"There is no directory under name {input_file_dir}")
-      return None
-    _files = []
+      return
+
+    files = []
     if (search_subdir):
       for (dirpath, dirnames, filenames) in os.walk(input_file_dir):
-        _files += [os.path.join(dirpath, file) for file in filenames]
+        files += [os.path.join(dirpath, file) for file in filenames]
     else:
-      _files = [os.path.join(input_file_dir, file) for file in os.listdir(input_file_dir)]
+      files = [os.path.join(input_file_dir, file) for file in os.listdir(input_file_dir)]
 
-    _new_sgfs_files = set([file for file in _files if file.split(".")[-1].lower() ==
+    new_sgfs_files = set([file for file in files if file.split(".")[-1].lower() ==
                            "sgfs"])
-    _new_sgf_files = set([file for file in _files if file.split(".")[-1].lower() == "sgf"])
+    new_sgf_files = set([file for file in files if file.split(".")[-1].lower() == "sgf"])
 
-    self._new_sgfs_files = _new_sgfs_files.difference(self._all_sgfs_files)
-    self._new_sgf_files = _new_sgf_files.difference(self._all_sgf_files)
+    #update the new_sgfs_files and sgf_files to remove duplication
+    new_sgfs_files = new_sgfs_files.difference(self._all_sgfs_files)
+    new_sgf_files = new_sgf_files.difference(self._all_sgf_files)
 
-    self._all_sgfs_files = self._all_sgfs_files.union(_new_sgfs_files)
-    self._all_sgf_files = self._all_sgf_files.union(_new_sgf_files)
-    print(f"We found {len(self._new_sgfs_files)} new sgfs files and {len(self._new_sgf_files)} new sgf files in the "
+    self._all_sgfs_files = self._all_sgfs_files.union(new_sgfs_files)
+    self._all_sgf_files = self._all_sgf_files.union(new_sgf_files)
+    print(f"We found {len(new_sgfs_files)} new sgfs files and {len(new_sgf_files)} new sgf files in the "
           f"search "
           f"directory {input_file_dir}.")
+    return {"new_sgfs_files": new_sgfs_files, "new_sgf_files": new_sgf_files}
 
   def _add_a_game_file_name(self, input_file_name):
+    new_sgf_files = []
+    new_sgfs_files =[]
     if not os.path.isfile(input_file_name):
       print(f"There is no file with name {input_file_name}.")
-      return None
+      return {"new_sgfs_files": new_sgfs_files, "new_sgf_files": new_sgf_files}
+
+    if input_file_name in self._all_sgfs_files or input_file_name in self._all_sgf_files:
+      print(f"File name {input_file_name} has been added before")
+      return {"new_sgfs_files": new_sgfs_files, "new_sgf_files": [new_sgf_files]}
+
     if input_file_name.split(".")[-1].lower() == "sgf":
-      self._new_sgf_files = {input_file_name}.difference(self._all_sgf_files)
-      if len(self._new_sgf_files) == 0:
-        print(f"File name {input_file_name} has been added before")
-        return None
-      else:
-        self._all_sgf_files = self._all_sgf_files.union({input_file_name})
+      new_sgf_files = [input_file_name]
+      self._all_sgf_files = self._all_sgf_files.union({input_file_name})
     elif input_file_name.split(".")[-1].lower() == "sgfs":
-      self._new_sgfs_files = {input_file_name}.difference(self._all_sgfs_files)
-      if len(self._new_sgfs_files) == 0:
-        print(f"File name {input_file_name} has been added before")
-        return None
-      else:
-        self._all_sgfs_files = self._all_sgfs_files.union({input_file_name})
+      new_sgfs_files = [input_file_name]
+      self._all_sgfs_files = self._all_sgfs_files.union({input_file_name})
     else:
       print(f"{input_file_name} is not sgf or sgfs file, no game was added")
-      return None
+      return
+    return {"new_sgfs_files": new_sgfs_files, "new_sgf_files": new_sgf_files}
 
-  def _add_new_games_to_result_dict(self):
+  def _add_new_games_to_result_dict(self, new_files):
     """add all sgfs files first"""
+    new_sgf_files = new_files["new_sgf_files"]
+    new_sgfs_files = new_files["new_sgfs_files"]
+
     idx = 1
-    for sgfs in self._new_sgfs_files:
+    for sgfs in new_sgfs_files:
       self._add_one_sgfs_file_to_result(sgfs)
       if (idx % 10 == 0):
-        print(f"Addedd {idx} files out of {len(self._new_sgfs_files)} sgfs files.")
+        print(f"Addedd {idx} files out of {len(new_sgfs_files)} sgfs files.")
       idx += 1
-    print(f"We have added additional {len(self._new_sgfs_files)} sgfs files into the results.")
+    #print(f"We have added additional {len(new_sgfs_files)} sgfs files into the results.")
 
     idx = 1
-    for sgf in self._new_sgf_files:
+    for sgf in new_sgf_files:
       self._add_one_sgf_file_to_result(sgf)
       if (idx % 10 == 0):
-        print(f"Added {idx} files out of {len(self._new_sgf_files)} sgf files.")
+        print(f"Added {idx} files out of {len(new_sgf_files)} sgf files.")
       idx += 1
-    print(f"We have added additional {len(self._new_sgf_files)} sgf files into the results.")
+    #print(f"We have added additional {len(new_sgf_files)} sgf files into the results.")
 
   def _add_one_sgfs_file_to_result(self, sgfs_file_name):
     """Add a single sgfs file. Each row of such sgf file contain a single game as a sgf string"""
     if not os.path.exists(sgfs_file_name):
       print(f"There is no SGFs file as {sgfs_file_name}")
-      return None
+      return
 
     with open(sgfs_file_name, "rb") as f:
       sgfs_strings = f.readlines()
@@ -188,7 +200,7 @@ class GameResultSummary:
     """Add a single sgf file."""
     if not os.path.exists(sgf_file_name):
       print(f"There is no SGF file as {sgf_file_name}")
-      return None
+      return
 
     with open(sgf_file_name, "rb") as f:
       sgf = f.read()
@@ -202,7 +214,7 @@ class GameResultSummary:
     pla_black = game.get_player_name('b')
     pla_white = game.get_player_name('w')
     if (game.get_handicap() is not None) or game.get_komi() < 5.5 or game.get_komi() > 7.5:
-      self._warning = True
+      self._should_warn_handicap_komi = True
 
     if (pla_black, pla_white) not in self.results:
       self.results[(pla_black, pla_white)] = Record()
@@ -218,17 +230,19 @@ class GameResultSummary:
     """Estimate and print elo values. This function must be called after add all the sgfs/sgf files"""
     pla_names = set(itertools.chain(*(name_pair for name_pair in self.results.keys())))
     data = []
-    for pla_Balck in pla_names:
-      for pla_White in pla_names:
-        if (pla_Balck == pla_White):
+    for pla_black in pla_names:
+      for pla_white in pla_names:
+        if (pla_black == pla_white):
           continue
         else:
-          record = self.results[(pla_Balck, pla_White)]
+          record = self.results[(pla_black, pla_white)]
           total = record.win + record.lost + record.draw
           win = record.win + 0.5 * record.draw
           winrate = win / total
-          data.extend(elo.likelihood_of_games(pla_Balck, pla_White, total, winrate, False))
-    data.extend(elo.make_center_elos_prior(list(pla_names), self._elo_prior))
+          data.extend(elo.likelihood_of_games(pla_black, pla_white, total, winrate, False))
+    for pla in pla_names:
+      data.extend(elo.make_single_player_prior(pla, self._elo_prior,0))
+    data.extend(elo.make_center_elos_prior(list(pla_names),0)) #add this in case user put elo_prior = 0
     info = elo.compute_elos(data, verbose=True)
     return info
 
@@ -279,7 +293,8 @@ if __name__ == "__main__":
                       nargs='+')
   parser.add_argument('-search-subdir', help='If we also need to search all sub-directories', required=False,
                       default=False)
-  parser.add_argument('-elo-prior', help='A Bayesian prior that the mean of all player Elos is the specified Elo ',
+  parser.add_argument('-elo-prior', help='Prior for Bayes Elo calculation, using input as the prior number of games '
+                                         'to stabilize the results',
                       required=False, type=float, default=0)
   args = vars(parser.parse_args())
 
@@ -287,9 +302,9 @@ if __name__ == "__main__":
   search_subdir = args["search_subdir"]
   elo_prior = args["elo_prior"]
 
-  gameResultSummary = GameResultSummary(elo_prior)
+  game_result_summary = GameResultSummary(elo_prior)
   for dir in input_dirs:
-    gameResultSummary.add_games(dir, search_subdir)
+    game_result_summary.add_games(dir, search_subdir)
 
-  gameResultSummary.print_game_results()
-  gameResultSummary.print_elos()
+  game_result_summary.print_game_results()
+  game_result_summary.print_elos()
