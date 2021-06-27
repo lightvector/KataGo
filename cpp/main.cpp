8aa461500fada9caee552b05671a52089d08f460
David Wu <lightvector@gmail.com>
2021-06-27 17:37:42 +0000

    Bump version to 1.9.0

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -178,11 +178,11 @@ int main(int argc, const char* argv[]) {
 
 
 string Version::getKataGoVersion() {
-  return string("1.8.2");
+  return string("1.9.0");
 }
 
 string Version::getKataGoVersionForHelp() {
-  return string("KataGo v1.8.2");
+  return string("KataGo v1.9.0");
 }
 
 string Version::getKataGoVersionFullInfo() {
