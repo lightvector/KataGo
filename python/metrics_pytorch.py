Fix softplus tail via custom backward gradient, shift stscore error scaling

python/metrics_pytorch.py
@@ -228,7 +228,8 @@ class Metrics:
     def loss_variance_time_samplewise(self, pred, target, weight, global_weight):
         assert pred.shape == (self.n,)
         assert target.shape == (self.n,)
-        loss = huber_loss(pred, target, delta = 50.0)
+        # Even if the training target is 0, add a tiny bit of irreducible error for regularizing the prediction.
+        loss = huber_loss(pred, target + 1.0e-5, delta = 50.0)
         return 0.0003 * global_weight * weight * loss
 
 
@@ -236,14 +237,16 @@ class Metrics:
         td_value_pred_probs = torch.softmax(td_value_pred_logits[:,2,:],axis=1)
         predvalue = (td_value_pred_probs[:,0] - td_value_pred_probs[:,1]).detach()
         realvalue = td_value_target_probs[:,2,0] - td_value_target_probs[:,2,1]
-        sqerror = torch.square(predvalue-realvalue)
+        # Even if the training target is 0, add a tiny bit of irreducible error for regularizing the prediction, 0.01%.
+        sqerror = torch.square(predvalue-realvalue) + 1.0e-8
         loss = huber_loss(pred, sqerror, delta = 0.4)
         return 2.0 * global_weight * weight * loss
 
     def loss_shortterm_score_error_samplewise(self, pred, td_score_pred, td_score_target, weight, global_weight):
         predscore = td_score_pred[:,2].detach()
         realscore = td_score_target[:,2]
-        sqerror = torch.square(predscore-realscore)
+        # Even if the training target is 0, add a tiny bit of irreducible error for regularizing the prediction, one hundredth of a point.
+        sqerror = torch.square(predscore-realscore) + 1.0e-4
         loss = huber_loss(pred, sqerror, delta = 100.0)
         return 0.00002 * global_weight * weight * loss
 

python/model_pytorch.py
@@ -61,6 +61,27 @@ def init_weights(tensor, activation, scale, fan_tensor=None):
     else:
         torch.nn.init.trunc_normal_(tensor, mean=0.0, std=std, a=-2.0*std, b=2.0*std)
 
+class SoftPlusWithGradientFloorFunction(torch.autograd.Function):
+    """
+    Same as softplus, except on backward pass, we never let the gradient decrease below grad_floor.
+    Equivalent to having a dynamic learning rate depending on stop_grad(x) where x is the input.
+    """
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, grad_floor: float):
+        ctx.save_for_backward(x)
+        ctx.grad_floor = grad_floor # grad_floor is not a tensor
+        return torch.nn.functional.softplus(x)
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        (x,) = ctx.saved_tensors
+        grad_floor = ctx.grad_floor
+        grad_x = None
+        grad_grad_floor = None
+        if ctx.needs_input_grad[0]:
+          grad_x = grad_output * (grad_floor + (1.0 - grad_floor) / (1.0 + torch.exp(-x)))
+        return grad_x, grad_grad_floor
+
 class BiasMask(torch.nn.Module):
     def __init__(
         self,
@@ -1693,11 +1714,11 @@ class Model(torch.nn.Module):
         futurepos_pretanh = out_futurepos
         seki_logits = out_seki
         pred_scoremean = out_miscvalue[:, 0] * 20.0
-        pred_scorestdev = torch.nn.functional.softplus(out_miscvalue[:, 1]) * 20.0
+        pred_scorestdev = SoftPlusWithGradientFloorFunction.apply(out_miscvalue[:, 1], 0.05) * 20.0
         pred_lead = out_miscvalue[:, 2] * 20.0
-        pred_variance_time = torch.nn.functional.softplus(out_miscvalue[:, 3]) * 40.0
-        pred_shortterm_value_error = torch.nn.functional.softplus(out_moremiscvalue[:,0]) * 0.25
-        pred_shortterm_score_error = torch.nn.functional.softplus(out_moremiscvalue[:,1]) * 30.0
+        pred_variance_time = SoftPlusWithGradientFloorFunction.apply(out_miscvalue[:, 3], 0.05) * 40.0
+        pred_shortterm_value_error = SoftPlusWithGradientFloorFunction.apply(out_moremiscvalue[:,0], 0.05) * 0.25
+        pred_shortterm_score_error = SoftPlusWithGradientFloorFunction.apply(out_moremiscvalue[:,1], 0.05) * 150.0
         scorebelief_logits = out_scorebelief_logprobs
 
         return (