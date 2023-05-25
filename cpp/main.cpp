ec6f2ad176e001ee7f2a51e7e0add6695bfb640b
David Wu <lightvector@gmail.com>
2023-05-25 15:03:34 -0400

    Version v1.13.2

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -206,11 +206,11 @@ int main(int argc, const char* const* argv) {
 
 
 string Version::getKataGoVersion() {
-  return string("1.13.1");
+  return string("1.13.2");
 }
 
 string Version::getKataGoVersionForHelp() {
-  return string("KataGo v1.13.1");
+  return string("KataGo v1.13.2");
 }
 
 string Version::getKataGoVersionFullInfo() {
