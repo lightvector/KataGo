3e82ae0508eead8d12b3a380a923db3408d385b4
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-07-02 13:48:17 +0800

    Initialize KataGo iOS project files

---
+++ ios/KataGo iOS/KataGo iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KataGo iOS
+//
+//  Created by Chin-Chang Yang on 2023/7/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/KataGo iOS/KataGo iOSUITests/KataGo_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KataGo_iOSUITestsLaunchTests.swift
+//  KataGo iOSUITests
+//
+//  Created by Chin-Chang Yang on 2023/7/2.
+//
+
+import XCTest
+
+final class KataGo_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
