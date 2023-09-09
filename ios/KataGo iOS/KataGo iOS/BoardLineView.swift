7ac342bce47217b6f0ae2dc0dff19f36a927eb4b
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-09-09 08:39:44 +0800

    Extract BoardLineView.swift from GobanView.swift.
    This commit adds the BoardLineView.swift file which contains the implementation for drawing the board lines and star points on the game board. The BoardLineView struct takes in the dimensions of the board, the board width and height, and uses SwiftUI to create and render the board lines and star points.
    
    The drawBoardBackground function draws the background texture of the board using the WoodImage.createTexture() method. The drawLines function uses the horizontalLine and verticalLine functions to draw the horizontal and vertical lines of the board, respectively.
    
    The drawStarPoint, drawStarPointsForSize, and drawStarPoints functions are responsible for drawing the star points on the board. The drawStarPoints function checks the dimensions of the board and draws the appropriate star points for a 19x19, 13x13, or 9x9 board.
    
    The BoardLineView_Previews struct is a preview provider for displaying the BoardLineView in a SwiftUI preview. It sets the dimensions of the board and creates an instance of the BoardLineView for previewing.

---
+++ ios/KataGo iOS/KataGo iOS/BoardLineView.swift
@@ -0,0 +1,99 @@
+//
+//  BoardLineView.swift
+//  KataGo iOS
+//
+//  Created by Chin-Chang Yang on 2023/9/9.
+//
+
+import SwiftUI
+
+struct BoardLineView: View {
+    let dimensions: Dimensions
+    let texture = WoodImage.createTexture()
+    let boardWidth: CGFloat
+    let boardHeight: CGFloat
+
+    var body: some View {
+        ZStack {
+            drawBoardBackground(texture: texture, dimensions: dimensions)
+            drawLines(dimensions: dimensions)
+            drawStarPoints(dimensions: dimensions)
+        }
+    }
+
+    private func drawBoardBackground(texture: UIImage, dimensions: Dimensions) -> some View {
+        Group {
+            Image(uiImage: texture)
+                .resizable()
+                .frame(width: (dimensions.boardWidth + dimensions.squareLength / 2), height: dimensions.boardHeight + (dimensions.squareLength / 2))
+        }
+    }
+
+    private func drawLines(dimensions: Dimensions) -> some View {
+        Group {
+            ForEach(0..<Int(boardHeight), id: \.self) { i in
+                horizontalLine(i: i, dimensions: dimensions)
+            }
+            ForEach(0..<Int(boardWidth), id: \.self) { i in
+                verticalLine(i: i, dimensions: dimensions)
+            }
+        }
+    }
+
+    private func horizontalLine(i: Int, dimensions: Dimensions) -> some View {
+        Path { path in
+            path.move(to: CGPoint(x: dimensions.marginWidth, y: dimensions.marginHeight + CGFloat(i) * dimensions.squareLength))
+            path.addLine(to: CGPoint(x: dimensions.marginWidth + dimensions.boardWidth - dimensions.squareLength, y: dimensions.marginHeight + CGFloat(i) * dimensions.squareLength))
+        }
+        .stroke(Color.black)
+    }
+
+    private func verticalLine(i: Int, dimensions: Dimensions) -> some View {
+        Path { path in
+            path.move(to: CGPoint(x: dimensions.marginWidth + CGFloat(i) * dimensions.squareLength, y: dimensions.marginHeight))
+            path.addLine(to: CGPoint(x: dimensions.marginWidth + CGFloat(i) * dimensions.squareLength, y: dimensions.marginHeight + dimensions.boardHeight - dimensions.squareLength))
+        }
+        .stroke(Color.black)
+    }
+
+    private func drawStarPoint(x: Int, y: Int, dimensions: Dimensions) -> some View {
+        // Big black dot
+        Circle()
+            .frame(width: dimensions.squareLength / 4, height: dimensions.squareLength / 4)
+            .foregroundColor(Color.black)
+            .position(x: dimensions.marginWidth + CGFloat(x) * dimensions.squareLength,
+                      y: dimensions.marginHeight + CGFloat(y) * dimensions.squareLength)
+    }
+
+    private func drawStarPointsForSize(points: [BoardPoint], dimensions: Dimensions) -> some View {
+        ForEach(points, id: \.self) { point in
+            drawStarPoint(x: point.x, y: point.y, dimensions: dimensions)
+        }
+    }
+
+    private func drawStarPoints(dimensions: Dimensions) -> some View {
+        Group {
+            if boardWidth == 19 && boardHeight == 19 {
+                // Draw star points for 19x19 board
+                drawStarPointsForSize(points: [BoardPoint(x: 3, y: 3), BoardPoint(x: 3, y: 9), BoardPoint(x: 3, y: 15), BoardPoint(x: 9, y: 3), BoardPoint(x: 9, y: 9), BoardPoint(x: 9, y: 15), BoardPoint(x: 15, y: 3), BoardPoint(x: 15, y: 9), BoardPoint(x: 15, y: 15)], dimensions: dimensions)
+            } else if boardWidth == 13 && boardHeight == 13 {
+                // Draw star points for 13x13 board
+                drawStarPointsForSize(points: [BoardPoint(x: 6, y: 6), BoardPoint(x: 3, y: 3), BoardPoint(x: 3, y: 9), BoardPoint(x: 9, y: 3), BoardPoint(x: 9, y: 9)], dimensions: dimensions)
+            } else if boardWidth == 9 && boardHeight == 9 {
+                // Draw star points for 9x9 board
+                drawStarPointsForSize(points: [BoardPoint(x: 4, y: 4), BoardPoint(x: 2, y: 2), BoardPoint(x: 2, y: 6), BoardPoint(x: 6, y: 2), BoardPoint(x: 6, y: 6)], dimensions: dimensions)
+            }
+        }
+    }
+}
+
+struct BoardLineView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReader { geometry in
+            let boardWidth: CGFloat = 13
+            let boardHeight: CGFloat = 13
+            let dimensions = Dimensions(geometry: geometry, width: boardWidth, height: boardHeight)
+            BoardLineView(dimensions: dimensions, boardWidth: boardWidth, boardHeight: boardHeight)
+        }
+    }
+}
