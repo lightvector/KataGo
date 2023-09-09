d8d3130c841d72d2a578d6cc088ef4a1a7f49e59
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-09-09 08:40:44 +0800

    Add BoardLineView.swift to the project and update GobanView to use it
    The BoardLineView.swift file has been added to the project and the GobanView has been updated to include the BoardLineView in the ZStack. This change reduces code complexity of GobanView.

--- ios/KataGo iOS/KataGo iOS/GobanView.swift
+++ ios/KataGo iOS/KataGo iOS/GobanView.swift
@@ -37,98 +37,29 @@ struct GobanView: View {
     let texture = WoodImage.createTexture()
 
     var body: some View {
-        VStack {
-            GeometryReader { geometry in
-                let dimensions = Dimensions(geometry: geometry, width: board.width, height: board.height)
-                ZStack {
-                    drawBoardBackground(texture: texture, dimensions: dimensions)
-                    drawLines(dimensions: dimensions)
-                    drawStarPoints(dimensions: dimensions)
-                    StoneView(dimensions: dimensions)
-                    AnalysisView(dimensions: dimensions)
-                }
+        GeometryReader { geometry in
+            let dimensions = Dimensions(geometry: geometry, width: board.width, height: board.height)
+            ZStack {
+                BoardLineView(dimensions: dimensions, boardWidth: board.width, boardHeight: board.height)
+                StoneView(dimensions: dimensions)
+                AnalysisView(dimensions: dimensions)
             }
-            .gesture(TapGesture().onEnded() { _ in
-                if nextPlayer.color == .black {
-                    KataGoHelper.sendCommand("genmove b")
-                    nextPlayer.color = .white
-                } else {
-                    KataGoHelper.sendCommand("genmove w")
-                    nextPlayer.color = .black
-                }
-
-                KataGoHelper.sendCommand("showboard")
-                KataGoHelper.sendCommand("kata-analyze interval 10")
-            })
         }
+        .gesture(TapGesture().onEnded() { _ in
+            if nextPlayer.color == .black {
+                KataGoHelper.sendCommand("genmove b")
+                nextPlayer.color = .white
+            } else {
+                KataGoHelper.sendCommand("genmove w")
+                nextPlayer.color = .black
+            }
+
+            KataGoHelper.sendCommand("showboard")
+            KataGoHelper.sendCommand("kata-analyze interval 10")
+        })
         .onAppear() {
             KataGoHelper.sendCommand("showboard")
             KataGoHelper.sendCommand("kata-analyze interval 10")
-        }
-    }
-
-    private func drawBoardBackground(texture: UIImage, dimensions: Dimensions) -> some View {
-        Group {
-            Image(uiImage: texture)
-                .resizable()
-                .frame(width: (dimensions.boardWidth + dimensions.squareLength / 2), height: dimensions.boardHeight + (dimensions.squareLength / 2))
-        }
-    }
-
-    private func drawLines(dimensions: Dimensions) -> some View {
-        Group {
-            ForEach(0..<Int(board.height), id: \.self) { i in
-                horizontalLine(i: i, dimensions: dimensions)
-            }
-            ForEach(0..<Int(board.width), id: \.self) { i in
-                verticalLine(i: i, dimensions: dimensions)
-            }
-        }
-    }
-
-    private func horizontalLine(i: Int, dimensions: Dimensions) -> some View {
-        Path { path in
-            path.move(to: CGPoint(x: dimensions.marginWidth, y: dimensions.marginHeight + CGFloat(i) * dimensions.squareLength))
-            path.addLine(to: CGPoint(x: dimensions.marginWidth + dimensions.boardWidth - dimensions.squareLength, y: dimensions.marginHeight + CGFloat(i) * dimensions.squareLength))
-        }
-        .stroke(Color.black)
-    }
-
-    private func verticalLine(i: Int, dimensions: Dimensions) -> some View {
-        Path { path in
-            path.move(to: CGPoint(x: dimensions.marginWidth + CGFloat(i) * dimensions.squareLength, y: dimensions.marginHeight))
-            path.addLine(to: CGPoint(x: dimensions.marginWidth + CGFloat(i) * dimensions.squareLength, y: dimensions.marginHeight + dimensions.boardHeight - dimensions.squareLength))
-        }
-        .stroke(Color.black)
-    }
-
-    private func drawStarPoint(x: Int, y: Int, dimensions: Dimensions) -> some View {
-        // Big black dot
-        Circle()
-            .frame(width: dimensions.squareLength / 4, height: dimensions.squareLength / 4)
-            .foregroundColor(Color.black)
-            .position(x: dimensions.marginWidth + CGFloat(x) * dimensions.squareLength,
-                      y: dimensions.marginHeight + CGFloat(y) * dimensions.squareLength)
-    }
-
-    private func drawStarPointsForSize(points: [BoardPoint], dimensions: Dimensions) -> some View {
-        ForEach(points, id: \.self) { point in
-            drawStarPoint(x: point.x, y: point.y, dimensions: dimensions)
-        }
-    }
-
-    private func drawStarPoints(dimensions: Dimensions) -> some View {
-        Group {
-            if board.width == 19 && board.height == 19 {
-                // Draw star points for 19x19 board
-                drawStarPointsForSize(points: [BoardPoint(x: 3, y: 3), BoardPoint(x: 3, y: 9), BoardPoint(x: 3, y: 15), BoardPoint(x: 9, y: 3), BoardPoint(x: 9, y: 9), BoardPoint(x: 9, y: 15), BoardPoint(x: 15, y: 3), BoardPoint(x: 15, y: 9), BoardPoint(x: 15, y: 15)], dimensions: dimensions)
-            } else if board.width == 13 && board.height == 13 {
-                // Draw star points for 13x13 board
-                drawStarPointsForSize(points: [BoardPoint(x: 6, y: 6), BoardPoint(x: 3, y: 3), BoardPoint(x: 3, y: 9), BoardPoint(x: 9, y: 3), BoardPoint(x: 9, y: 9)], dimensions: dimensions)
-            } else if board.width == 9 && board.height == 9 {
-                // Draw star points for 9x9 board
-                drawStarPointsForSize(points: [BoardPoint(x: 4, y: 4), BoardPoint(x: 2, y: 2), BoardPoint(x: 2, y: 6), BoardPoint(x: 6, y: 2), BoardPoint(x: 6, y: 6)], dimensions: dimensions)
-            }
         }
     }
 }
