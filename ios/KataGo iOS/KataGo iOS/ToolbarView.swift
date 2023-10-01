d2a434261dadf62bc2bc2661052b6f1549496a75
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-10-01 19:37:07 +0800

    Add ToolbarView.swift for iOS project.
    The ToolbarView struct in this file represents the toolbar view displayed in the KataGo iOS app. It contains several button actions that interact with the KataGo engine. These actions include undo, play move, start/stop analysis, and clear the board.
    
    The ToolbarView struct conforms to the SwiftUI View protocol and takes in environment objects for player state and app configuration. It also receives a binding for the isAnalyzing state.
    
    The ToolbarView_Previews struct is used for previewing the toolbar view in Xcode's preview canvas. It sets up the necessary environment objects and passes in a dummy value for the isAnalyzing binding.
    
    Overall, this commit adds the ToolbarView and its preview functionality to the iOS project.

---
+++ ios/KataGo iOS/KataGo iOS/ToolbarView.swift
@@ -0,0 +1,83 @@
+//
+//  ToolbarView.swift
+//  KataGo iOS
+//
+//  Created by Chin-Chang Yang on 2023/10/1.
+//
+
+import SwiftUI
+
+struct ToolbarView: View {
+    @EnvironmentObject var player: PlayerObject
+    @EnvironmentObject var config: Config
+    @Binding var isAnalyzing: Bool
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                KataGoHelper.sendCommand("undo")
+                KataGoHelper.sendCommand("showboard")
+                if isAnalyzing {
+                    KataGoHelper.sendCommand(config.getKataAnalyzeCommand())
+                }
+            }) {
+                Image(systemName: "arrow.uturn.backward")
+            }
+            .padding()
+
+            Button(action: {
+                let nextColor = (player.nextColorForPlayCommand == .black) ? "b" : "w"
+                let pass = "play \(nextColor) pass"
+                KataGoHelper.sendCommand(pass)
+                KataGoHelper.sendCommand("showboard")
+                if isAnalyzing {
+                    KataGoHelper.sendCommand(config.getKataAnalyzeCommand())
+                }
+            }) {
+                Image(systemName: "hand.raised")
+            }
+            .padding()
+
+            Button(action: {
+                if isAnalyzing {
+                    KataGoHelper.sendCommand(config.getKataAnalyzeCommand())
+                }
+            }) {
+                Image(systemName: "play")
+            }
+            .padding()
+
+            Button(action: {
+                if isAnalyzing {
+                    KataGoHelper.sendCommand("stop")
+                }
+            }) {
+                Image(systemName: "stop")
+            }
+            .padding()
+
+            Button(action: {
+                KataGoHelper.sendCommand("clear_board")
+                KataGoHelper.sendCommand("showboard")
+                if isAnalyzing {
+                    KataGoHelper.sendCommand(config.getKataAnalyzeCommand())
+                }
+            }) {
+                Image(systemName: "clear")
+            }
+            .padding()
+        }
+    }
+}
+
+struct ToolbarView_Previews: PreviewProvider {
+    static let player = PlayerObject()
+    static let config = Config()
+
+    static var previews: some View {
+        @State var isAnalyzing = true
+        ToolbarView(isAnalyzing: $isAnalyzing)
+            .environmentObject(player)
+            .environmentObject(config)
+    }
+}
