13f2c0be3d49c8d1a6598316aa02baa7fc148d1a
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-10-01 09:11:16 +0800

    Add KataGoModel.swift with board, stones, player, analysis, config, dimensions, and messages objects
    This commit adds the KataGoModel.swift file, which contains the implementation of various observable objects used in the KataGo iOS application. These objects include the ObservableBoard, Stones, PlayerObject, Analysis, Config, Dimensions, Message, and MessagesObject. The file also defines the PlayerColor and Ownership structs.

---
+++ ios/KataGo iOS/KataGo iOS/KataGoModel.swift
@@ -0,0 +1,108 @@
+//
+//  KataGoModel.swift
+//  KataGo iOS
+//
+//  Created by Chin-Chang Yang on 2023/10/1.
+//
+
+import SwiftUI
+
+class ObservableBoard: ObservableObject {
+    @Published var width: CGFloat = 19
+    @Published var height: CGFloat = 19
+}
+
+struct BoardPoint: Hashable, Comparable {
+    let x: Int
+    let y: Int
+
+    static func < (lhs: BoardPoint, rhs: BoardPoint) -> Bool {
+        return (lhs.y, lhs.x) < (rhs.y, rhs.x)
+    }
+}
+
+class Stones: ObservableObject {
+    @Published var blackPoints: [BoardPoint] = []
+    @Published var whitePoints: [BoardPoint] = []
+}
+
+enum PlayerColor {
+    case black
+    case white
+}
+
+class PlayerObject: ObservableObject {
+    @Published var nextColorForPlayCommand = PlayerColor.black
+    @Published var nextColorFromShowBoard = PlayerColor.black
+}
+
+struct Ownership {
+    let mean: Float
+    let stdev: Float?
+
+    init(mean: Float, stdev: Float?) {
+        self.mean = mean
+        self.stdev = stdev
+    }
+}
+
+class Analysis: ObservableObject {
+    @Published var nextColorForAnalysis = PlayerColor.white
+    @Published var data: [[String: String]] = []
+    @Published var ownership: [BoardPoint: Ownership] = [:]
+}
+
+class Config: ObservableObject {
+    @Published var maxMessageCharacters: Int = defaultMaxMessageCharacters
+    @Published var maxAnalysisMoves: Int = defaultMaxAnalysisMoves
+}
+
+extension Config {
+    static let defaultMaxMessageCharacters = 200
+    static let defaultMaxAnalysisMoves = 8
+}
+
+struct Dimensions {
+    let squareLength: CGFloat
+    let boardWidth: CGFloat
+    let boardHeight: CGFloat
+    let marginWidth: CGFloat
+    let marginHeight: CGFloat
+
+    init(geometry: GeometryProxy, board: ObservableBoard) {
+        self.init(geometry: geometry, width: board.width, height: board.height)
+    }
+
+    private init(geometry: GeometryProxy, width: CGFloat, height: CGFloat) {
+        let totalWidth = geometry.size.width
+        let totalHeight = geometry.size.height
+        let squareWidth = totalWidth / (width + 1)
+        let squareHeight = totalHeight / (height + 1)
+        squareLength = min(squareWidth, squareHeight)
+        boardWidth = width * squareLength
+        boardHeight = height * squareLength
+        marginWidth = (totalWidth - boardWidth + squareLength) / 2
+        marginHeight = (totalHeight - boardHeight + squareLength) / 2
+    }
+}
+
+/// Message with a text and an ID
+struct Message: Identifiable, Equatable, Hashable {
+    /// Identification of this message
+    let id = UUID()
+
+    /// Text of this message
+    let text: String
+
+    /// Initialize a message with a text and a max length
+    /// - Parameters:
+    ///   - text: a text
+    ///   - maxLength: a max length
+    init(text: String, maxLength: Int) {
+        self.text = String(text.prefix(maxLength))
+    }
+}
+
+class MessagesObject: ObservableObject {
+    @Published var messages: [Message] = []
+}
