b8efc935041b9153b5b5a8b14f8b4d4fb91cdf35
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-10-03 19:04:57 +0800

    EditButtonBar and ConfigItems for ConfigView
    - Refactored ConfigView into two new subviews: EditButtonBar and ConfigItems for better organization
    - Implemented EditButtonBar to display an EditButton on the top-right corner of the view
    - Implemented ConfigItem subview to display configuration items with editable and non-editable fields
    - Updated the layout of ConfigView to incorporate the new subviews and make use of VStack and HStacks for proper alignment and spacing

--- ios/KataGo iOS/KataGo iOS/ConfigView.swift
+++ ios/KataGo iOS/KataGo iOS/ConfigView.swift
@@ -7,28 +7,71 @@
 
 import SwiftUI
 
-struct ConfigView: View {
+struct EditButtonBar: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            EditButton()
+        }
+    }
+}
+
+struct ConfigItem: View {
+    @Environment(\.editMode) private var editMode
+    let title: String
+    @Binding var content: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+            Spacer()
+            if editMode?.wrappedValue.isEditing == true {
+                TextField("", text: $content)
+                    .multilineTextAlignment(.trailing)
+                    .background(Color(white: 0.9))
+            } else {
+                Text(content)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct ConfigItems: View {
     @EnvironmentObject var config: Config
     @State var maxMessageCharacters: String = "200"
     @State var maxAnalysisMoves: String = "8"
 
     var body: some View {
         VStack {
-            HStack {
-                Text("Max message characters:")
-                TextField("200", text: $maxMessageCharacters)
-            }
+            ConfigItem(title: "Max message characters:", content: $maxMessageCharacters)
+                .onChange(of: maxMessageCharacters) { newText in
+                    config.maxMessageCharacters = Int(newText) ??
+                    Config.defaultMaxMessageCharacters
+                }
+                .padding(.bottom)
 
-            HStack {
-                Text("Max analysis moves:")
-                TextField("8", text: $maxAnalysisMoves)
-            }
+            ConfigItem(title: "Max analysis moves:", content: $maxAnalysisMoves)
+                .onChange(of: maxAnalysisMoves) { newText in
+                    config.maxAnalysisMoves = Int(newText) ??
+                    Config.defaultMaxAnalysisMoves
+                }
         }
-        .padding()
-        .onDisappear() {
-            config.maxMessageCharacters = Int(maxMessageCharacters) ?? Config.defaultMaxMessageCharacters
-            config.maxAnalysisMoves = Int(maxAnalysisMoves) ?? Config.defaultMaxAnalysisMoves
+    }
+}
+
+struct ConfigView: View {
+    @State var isEditing = EditMode.inactive
+
+    var body: some View {
+        VStack {
+            EditButtonBar()
+                .padding()
+            ConfigItems()
+                .padding()
         }
+        .frame(maxHeight: .infinity, alignment: .topLeading)
+        .environment(\.editMode, $isEditing)
     }
 }
 
