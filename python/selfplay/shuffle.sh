694e7561829719c7ae4d14aa3edee79852adc454
David Wu <lightvector@gmail.com>
2023-11-14 19:23:13 -0500

    Reduce shuffle backups

--- python/selfplay/shuffle.sh
+++ python/selfplay/shuffle.sh
@@ -107,11 +107,11 @@ mv -Tf "$BASEDIR"/shuffleddata/current_tmp "$BASEDIR"/shuffleddata/current
 
 # CLEANUP ---------------------------------------------------------------
 
-#Among shuffled dirs older than 2 hours, remove all but the most recent 5 of them.
+#Among shuffled dirs older than 2 hours, remove all but the most recent 2 of them.
 #This should be VERY conservative and allow plenty of time for the training to switch
 #to newer ones as they get generated.
 echo "Cleaning up any old dirs"
-find "$BASEDIR"/shuffleddata/ -mindepth 1 -maxdepth 1 -type d -mmin +120 | sort | head -n -5 | xargs --no-run-if-empty rm -r
+find "$BASEDIR"/shuffleddata/ -mindepth 1 -maxdepth 1 -type d -mmin +120 | sort | head -n -2 | xargs --no-run-if-empty rm -r
 
 echo "Finished shuffle at" $(date "+%Y-%m-%d %H:%M:%S")
 #Make a little space between shuffles
