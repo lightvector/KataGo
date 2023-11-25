c9f44c68bde00adcd7770b4c16678fd877adfd08
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-11-25 17:24:43 +0800

    Add setup script to configure KataGo models
    This commit adds a new setup script that automates the download and
    configuration of the KataGo models needed for the project. The script
    downloads the model binary and the machine learning package, extracts
    them into the appropriate directory, and creates symbolic links to the
    necessary files. By providing an automated setup process, this change
    simplifies the rebuilding process for new contributors and ensures
    consistent model setup across different environments.

---
+++ cpp/xcode/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+wget https://github.com/ChinChangYang/KataGo/releases/download/v1.13.2-coreml1/kata1-b18c384nbt-s7709731328-d3715293823.bin.gz
+mv kata1-b18c384nbt-s7709731328-d3715293823.bin.gz DerivedData/KataGo/Build/Products/Debug/model.bin.gz
+wget https://github.com/ChinChangYang/KataGo/releases/download/v1.13.2-coreml1/KataGoModel19x19fp16v14s7709731328.mlpackage.zip
+mv KataGoModel19x19fp16v14s7709731328.mlpackage.zip DerivedData/KataGo/Build/Products/Debug/
+unzip DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage.zip -d DerivedData/KataGo/Build/Products/Debug/
+mv DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16.mlpackage
+ln -s ../../../../../../configs/misc/coreml_example.cfg DerivedData/KataGo/Build/Products/Debug/gtp.cfg
+ln -s ../../../../../../tests DerivedData/KataGo/Build/Products/Debug/tests
+ln -s ../Debug/model.bin.gz DerivedData/KataGo/Build/Products/Release/
+ln -s ../Debug/KataGoModel19x19fp16.mlpackage DerivedData/KataGo/Build/Products/Release/
+ln -s ../Debug/gtp.cfg DerivedData/KataGo/Build/Products/Release/
