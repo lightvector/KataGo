35144676488c92ecf08d3bc549136e228e09ec71
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-12-10 21:17:49 +0800

    Remove unnecessary test methods from testnn.mm

--- cpp/xcode/KataGoTest/testnn.mm
+++ cpp/xcode/KataGoTest/testnn.mm
@@ -19,43 +19,6 @@
     MainCmds::runnnlayertests(args);
 }
 
-- (void)testOutput {
-    std::vector<std::string> args;
-    MainCmds::runoutputtests(args);
-}
-
-- (void)testNNOnTinyBoard {
-    std::vector<std::string> args;
-    args.push_back("katago");
-    args.push_back("model.bin.gz");
-    args.push_back("false");
-    args.push_back("false");
-    args.push_back("0");
-    args.push_back("false");
-    MainCmds::runnnontinyboardtest(args);
-}
-
-- (void)testNNOnTinyBoardFp16 {
-    std::vector<std::string> args;
-    args.push_back("katago");
-    args.push_back("model.bin.gz");
-    args.push_back("false");
-    args.push_back("false");
-    args.push_back("0");
-    args.push_back("true");
-    MainCmds::runnnontinyboardtest(args);
-}
-
-- (void)testNNSymmetries {
-    std::vector<std::string> args;
-    args.push_back("katago");
-    args.push_back("model.bin.gz");
-    args.push_back("false");
-    args.push_back("false");
-    args.push_back("false");
-    MainCmds::runnnsymmetriestest(args);
-}
-
 - (void)testOwnership {
     std::vector<std::string> args;
     args.push_back("katago");
