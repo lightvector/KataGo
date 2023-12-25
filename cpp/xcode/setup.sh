c103ed3342e6f048b68aa1580d23a91ca92d9f38
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2023-12-25 09:43:57 +0800

    Refactor setup script to streamline model deployment
    - Remove old `KataGoModel19x19fp16.mlpackage`.

--- cpp/xcode/setup.sh
+++ cpp/xcode/setup.sh
@@ -4,6 +4,7 @@ mv kata1-b18c384nbt-s7709731328-d3715293823.bin.gz DerivedData/KataGo/Build/Prod
 wget https://github.com/ChinChangYang/KataGo/releases/download/v1.13.2-coreml1/KataGoModel19x19fp16v14s7709731328.mlpackage.zip
 mv KataGoModel19x19fp16v14s7709731328.mlpackage.zip DerivedData/KataGo/Build/Products/Debug/
 unzip DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage.zip -d DerivedData/KataGo/Build/Products/Debug/
+rm -rf DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16.mlpackage
 mv DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16.mlpackage
 ln -s ../../../../../../configs/misc/coreml_example.cfg DerivedData/KataGo/Build/Products/Debug/gtp.cfg
 ln -s ../../../../../../tests DerivedData/KataGo/Build/Products/Debug/tests
