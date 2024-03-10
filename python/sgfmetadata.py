ab2b85ca763e89cc04c53cc4c3026bda9278cea7
David Wu <lightvector@gmail.com>
2024-03-10 12:47:18 -0400

    Add python version of sgfmetadata dataclass

---
+++ python/sgfmetadata.py
@@ -0,0 +1,169 @@
+# See the equivalents in trainingwrite.h and trainingwrite.cpp in the cpp directory.
+# Translated from the c++ implementation.
+
+from dataclasses import dataclass
+from typing import ClassVar
+import datetime
+import math
+import numpy as np
+import random
+
+@dataclass
+class SGFMetadata:
+    inverseBRank: int = 0
+    inverseWRank: int = 0
+    bIsUnranked: bool = False
+    wIsUnranked: bool = False
+    bRankIsUnknown: bool = False
+    wRankIsUnknown: bool = False
+    bIsHuman: bool = False
+    wIsHuman: bool = False
+
+    gameIsUnrated: bool = False
+    gameRatednessIsUnknown: bool = False
+
+    tcIsUnknown: bool = False
+    tcIsNone: bool = False
+    tcIsAbsolute: bool = False
+    tcIsSimple: bool = False
+    tcIsByoYomi: bool = False
+    tcIsCanadian: bool = False
+    tcIsFischer: bool = False
+
+    mainTimeSeconds: float = 0.0
+    periodTimeSeconds: float = 0.0
+    byoYomiPeriods: int = 0
+    canadianMoves: int = 0
+
+    boardArea: int = 0
+
+    gameDate: datetime.date = datetime.date(1970, 1, 1)
+
+    source: int = 0
+
+    SOURCE_OGS: ClassVar[int] = 1
+    SOURCE_KGS: ClassVar[int] = 2
+    SOURCE_FOX: ClassVar[int] = 3
+    SOURCE_TYGEM: ClassVar[int] = 4
+    SOURCE_GOGOD: ClassVar[int] = 5
+    SOURCE_GO4GO: ClassVar[int] = 6
+
+    METADATA_INPUT_NUM_CHANNELS: ClassVar[int] = 192
+
+    @classmethod
+    def get_katago_selfplay_metadata(cls, board_area: int, rand: random.Random) -> "SGFMetadata":
+        return SGFMetadata(
+            inverseBRank = 0,
+            inverseWRank = 0,
+            bIsUnranked = False,
+            wIsUnranked = False,
+            bRankIsUnknown = False,
+            wRankIsUnknown = False,
+            bIsHuman = False,
+            wIsHuman = False,
+
+            gameIsUnrated = False,
+            gameRatednessIsUnknown = False,
+
+            tcIsUnknown = False,
+            tcIsNone = False,
+            tcIsAbsolute = False,
+            tcIsSimple = False,
+            tcIsByoYomi = True,
+            tcIsCanadian = False,
+            tcIsFischer = False,
+
+            mainTimeSeconds = rand.choice([300,600,900,1200]),
+            periodTimeSeconds = rand.choice([10.0,15.0,30.0]),
+            byoYomiPeriods = 5,
+            canadianMoves = 0,
+
+            boardArea = board_area,
+            gameDate = datetime.date(2022, 1, 1) + datetime.timedelta(days=rand.randint(0,722)),
+            source = 0,
+        )
+
+
+    def get_metadata_row(self, nextPlayer: str) -> np.ndarray:
+        assert sgfMeta is not None
+        nextPlayer = nextPlayer.lower()
+        rowMetadata = np.zeros(self.METADATA_INPUT_NUM_CHANNELS, dtype=np.float32)
+
+        plaIsHuman = self.wIsHuman if nextPlayer == "w" else self.bIsHuman
+        oppIsHuman = self.bIsHuman if nextPlayer == "w" else self.wIsHuman
+        rowMetadata[0] = 1.0 if plaIsHuman else 0.0
+        rowMetadata[1] = 1.0 if oppIsHuman else 0.0
+
+        plaIsUnranked = self.wIsUnranked if nextPlayer == "w" else self.bIsUnranked
+        oppIsUnranked = self.bIsUnranked if nextPlayer == "w" else self.wIsUnranked
+        rowMetadata[2] = 1.0 if plaIsUnranked else 0.0
+        rowMetadata[3] = 1.0 if oppIsUnranked else 0.0
+
+        plaRankIsUnknown = self.wRankIsUnknown if nextPlayer == "w" else self.bRankIsUnknown
+        oppRankIsUnknown = self.bRankIsUnknown if nextPlayer == "w" else self.wRankIsUnknown
+        rowMetadata[4] = 1.0 if plaRankIsUnknown else 0.0
+        rowMetadata[5] = 1.0 if oppRankIsUnknown else 0.0
+
+        RANK_START_IDX = 6
+        invPlaRank = self.inverseWRank if nextPlayer == "w" else self.inverseBRank
+        invOppRank = self.inverseBRank if nextPlayer == "w" else self.inverseWRank
+        RANK_LEN_PER_PLA = 34
+        if not plaIsUnranked:
+            for i in range(min(invPlaRank, RANK_LEN_PER_PLA)):
+                rowMetadata[RANK_START_IDX + i] = 1.0
+        if not oppIsUnranked:
+            for i in range(min(invOppRank, RANK_LEN_PER_PLA)):
+                rowMetadata[RANK_START_IDX + RANK_LEN_PER_PLA + i] = 1.0
+
+        assert RANK_START_IDX + 2 * RANK_LEN_PER_PLA == 74
+        rowMetadata[74] = 0.5 if self.gameRatednessIsUnknown else 1.0 if self.gameIsUnrated else 0.0
+
+        rowMetadata[75] = 1.0 if self.tcIsUnknown else 0.0
+        rowMetadata[76] = 1.0 if self.tcIsNone else 0.0
+        rowMetadata[77] = 1.0 if self.tcIsAbsolute else 0.0
+        rowMetadata[78] = 1.0 if self.tcIsSimple else 0.0
+        rowMetadata[79] = 1.0 if self.tcIsByoYomi else 0.0
+        rowMetadata[80] = 1.0 if self.tcIsCanadian else 0.0
+        rowMetadata[81] = 1.0 if self.tcIsFischer else 0.0
+        assert (
+            rowMetadata[75]
+            + rowMetadata[76]
+            + rowMetadata[77]
+            + rowMetadata[78]
+            + rowMetadata[79]
+            + rowMetadata[80]
+            + rowMetadata[81]
+            == 1.0
+        )
+
+        mainTimeSecondsCapped = min(max(self.mainTimeSeconds, 0.0), 3.0 * 86400)
+        periodTimeSecondsCapped = min(max(self.periodTimeSeconds, 0.0), 1.0 * 86400)
+        rowMetadata[82] = 0.4 * (math.log(mainTimeSecondsCapped + 60.0) - 6.5)
+        rowMetadata[83] = 0.3 * (math.log(periodTimeSecondsCapped + 1.0) - 3.0)
+        byoYomiPeriodsCapped = min(max(self.byoYomiPeriods, 0), 50)
+        canadianMovesCapped = min(max(self.canadianMoves, 0), 50)
+        rowMetadata[84] = 0.5 * (math.log(byoYomiPeriodsCapped + 2.0) - 1.5)
+        rowMetadata[85] = 0.25 * (math.log(canadianMovesCapped + 2.0) - 1.5)
+
+        rowMetadata[86] = 0.5 * math.log(self.boardArea / 361.0)
+
+        daysDifference = (self.gameDate - datetime.date(1970, 1, 1)).days
+        DATE_START_IDX = 87
+        DATE_LEN = 32
+        period = 7.0
+        factor = pow(80000.0, 1.0 / (DATE_LEN - 1))
+        twopi = 6.283185307179586476925
+        for i in range(DATE_LEN):
+            numRevolutions = daysDifference / period
+            rowMetadata[DATE_START_IDX + i * 2 + 0] = math.cos(numRevolutions * twopi)
+            rowMetadata[DATE_START_IDX + i * 2 + 1] = math.sin(numRevolutions * twopi)
+            period *= factor
+
+        assert DATE_START_IDX + 2 * DATE_LEN == 151
+
+        assert 0 <= self.source < 16
+        rowMetadata[151 + self.source] = 1.0
+
+        assert 151 + 16 < self.METADATA_INPUT_NUM_CHANNELS
+
+        return rowMetadata
