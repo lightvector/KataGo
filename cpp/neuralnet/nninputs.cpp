01571d5e11f8932ab787fb6815a74761275520d5
David Wu <lightvector@gmail.com>
2024-04-13 22:54:04 -0400

    Move sgfmetadata to nninputs

--- cpp/neuralnet/nninputs.cpp
+++ cpp/neuralnet/nninputs.cpp
@@ -835,6 +835,87 @@ void SymmetryHelpers::getSymmetryDifferences(
   }
 }
 
+//-------------------------------------------------------------------------------------------------------------
+
+void SGFMetadata::fillMetadataRow(SGFMetadata* sgfMeta, float* rowMetadata, Player nextPlayer) {
+  assert(sgfMeta != NULL);
+
+  for(int i = 0; i<SGFMetadata::METADATA_INPUT_NUM_CHANNELS; i++)
+    rowMetadata[i] = 0.0f;
+
+  bool plaIsHuman = (nextPlayer == P_WHITE) ? sgfMeta->wIsHuman : sgfMeta->bIsHuman;
+  bool oppIsHuman = (nextPlayer == P_WHITE) ? sgfMeta->bIsHuman : sgfMeta->wIsHuman;
+  rowMetadata[0] = plaIsHuman ? 1.0f : 0.0f;
+  rowMetadata[1] = oppIsHuman ? 1.0f : 0.0f;
+
+  bool plaIsUnranked = (nextPlayer == P_WHITE) ? sgfMeta->wIsUnranked : sgfMeta->bIsUnranked;
+  bool oppIsUnranked = (nextPlayer == P_WHITE) ? sgfMeta->bIsUnranked : sgfMeta->wIsUnranked;
+  rowMetadata[2] = plaIsUnranked ? 1.0f : 0.0f;
+  rowMetadata[3] = oppIsUnranked ? 1.0f : 0.0f;
+
+  bool plaRankIsUnknown = (nextPlayer == P_WHITE) ? sgfMeta->wRankIsUnknown : sgfMeta->bRankIsUnknown;
+  bool oppRankIsUnknown = (nextPlayer == P_WHITE) ? sgfMeta->bRankIsUnknown : sgfMeta->wRankIsUnknown;
+  rowMetadata[4] = plaRankIsUnknown ? 1.0f : 0.0f;
+  rowMetadata[5] = oppRankIsUnknown ? 1.0f : 0.0f;
+
+  static constexpr int RANK_START_IDX = 6;
+  int invPlaRank = (nextPlayer == P_WHITE) ? sgfMeta->inverseWRank : sgfMeta->inverseBRank;
+  int invOppRank = (nextPlayer == P_WHITE) ? sgfMeta->inverseBRank : sgfMeta->inverseWRank;
+  static constexpr int RANK_LEN_PER_PLA = 34;
+  if(!plaIsUnranked) {
+    for(int i = 0; i<std::min(invPlaRank,RANK_LEN_PER_PLA); i++)
+      rowMetadata[RANK_START_IDX + i] = 1.0f;
+  }
+  if(!oppIsUnranked) {
+    for(int i = 0; i<std::min(invOppRank,RANK_LEN_PER_PLA); i++)
+      rowMetadata[RANK_START_IDX + RANK_LEN_PER_PLA + i] = 1.0f;
+  }
+
+  static_assert(74 == RANK_START_IDX + 2 * RANK_LEN_PER_PLA, "");
+  rowMetadata[74] = sgfMeta->gameRatednessIsUnknown ? 0.5f : sgfMeta->gameIsUnrated ? 1.0f : 0.0f;
+
+  rowMetadata[75] = sgfMeta->tcIsUnknown ? 1.0f : 0.0f;
+  rowMetadata[76] = sgfMeta->tcIsNone ? 1.0f : 0.0f;
+  rowMetadata[77] = sgfMeta->tcIsAbsolute ? 1.0f : 0.0f;
+  rowMetadata[78] = sgfMeta->tcIsSimple ? 1.0f : 0.0f;
+  rowMetadata[79] = sgfMeta->tcIsByoYomi ? 1.0f : 0.0f;
+  rowMetadata[80] = sgfMeta->tcIsCanadian ? 1.0f : 0.0f;
+  rowMetadata[81] = sgfMeta->tcIsFischer ? 1.0f : 0.0f;
+  assert(rowMetadata[75] + rowMetadata[76] + rowMetadata[77] + rowMetadata[78] + rowMetadata[79] + rowMetadata[80] + rowMetadata[81] == 1.0f);
+
+  double mainTimeSecondsCapped = std::min(std::max(sgfMeta->mainTimeSeconds,0.0),3.0*86400);
+  double periodTimeSecondsCapped = std::min(std::max(sgfMeta->periodTimeSeconds,0.0),1.0*86400);
+  rowMetadata[82] = (float)(0.4 * (log(mainTimeSecondsCapped + 60.0) - 6.5));
+  rowMetadata[83] = (float)(0.3 * (log(periodTimeSecondsCapped + 1.0) - 3.0));
+  int byoYomiPeriodsCapped = std::min(std::max(sgfMeta->byoYomiPeriods,0),50);
+  int canadianMovesCapped = std::min(std::max(sgfMeta->canadianMoves,0),50);
+  rowMetadata[84] = (float)(0.5 * (log(byoYomiPeriodsCapped + 2.0) - 1.5));
+  rowMetadata[85] = (float)(0.25 * (log(canadianMovesCapped + 2.0) - 1.5));
+
+  rowMetadata[86] = (float)(0.5 * log(sgfMeta->boardArea/361.0));
+
+  double daysDifference = sgfMeta->gameDate.numDaysAfter(SimpleDate(1970,1,1));
+  static constexpr int DATE_START_IDX = 87;
+  static constexpr int DATE_LEN = 32;
+  // 7 because we're curious if there's a day-of-the-week effect
+  // on gameplay...
+  double period = 7.0;
+  static const double factor = pow(80000, 1.0/(DATE_LEN-1));
+  static constexpr double twopi = 6.283185307179586476925;
+  for(int i = 0; i<DATE_LEN; i++) {
+    double numRevolutions = daysDifference / period;
+    rowMetadata[DATE_START_IDX + i*2 + 0] = (float)(cos(numRevolutions * twopi));
+    rowMetadata[DATE_START_IDX + i*2 + 1] = (float)(sin(numRevolutions * twopi));
+    period *= factor;
+  }
+  static_assert(151 == DATE_START_IDX + 2 * DATE_LEN, "");
+
+  assert(sgfMeta->source >= 0 && sgfMeta->source < 16);
+  rowMetadata[151 + sgfMeta->source] = 1.0f;
+
+  static_assert(151 + 16 < SGFMetadata::METADATA_INPUT_NUM_CHANNELS, "");
+}
+
 
 
 //-------------------------------------------------------------------------------------------------------------
--- cpp/neuralnet/nninputs.h
+++ cpp/neuralnet/nninputs.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "../core/global.h"
+#include "../core/datetime.h"
 #include "../core/hash.h"
 #include "../core/rand.h"
 #include "../game/board.h"
@@ -54,6 +55,51 @@ struct MiscNNInputParams {
   static const Hash128 ZOBRIST_POLICY_OPTIMISM;
   static const Hash128 ZOBRIST_ZERO_HISTORY;
 };
+
+struct SGFMetadata {
+  int inverseBRank = 0; // KG = 0, 9d = 1, 8d = 2,... 1d = 9, 1k = 10, 2k = 11, ...
+  int inverseWRank = 0;
+  bool bIsUnranked = false; // KGS "-" rank
+  bool wIsUnranked = false;
+  bool bRankIsUnknown = false; // No rank in file
+  bool wRankIsUnknown = false;
+  bool bIsHuman = false;
+  bool wIsHuman = false;
+
+  bool gameIsUnrated = false;
+  bool gameRatednessIsUnknown = false;
+
+  //One-hot for all things with metadata
+  bool tcIsUnknown = false;
+  bool tcIsNone = false;
+  bool tcIsAbsolute = false;
+  bool tcIsSimple = false;
+  bool tcIsByoYomi = false;
+  bool tcIsCanadian = false;
+  bool tcIsFischer = false;
+
+  double mainTimeSeconds = 0.0;
+  double periodTimeSeconds = 0.0;
+  int byoYomiPeriods = 0;
+  int canadianMoves = 0;
+
+  int boardArea = 0;
+
+  SimpleDate gameDate;
+
+  int source = 0;
+  static const int SOURCE_OGS = 1;
+  static const int SOURCE_KGS = 2;
+  static const int SOURCE_FOX = 3;
+  static const int SOURCE_TYGEM = 4;
+  static const int SOURCE_GOGOD = 5;
+  static const int SOURCE_GO4GO = 6;
+
+  static constexpr int METADATA_INPUT_NUM_CHANNELS = 192;
+
+  static void fillMetadataRow(SGFMetadata* sgfMeta, float* rowMetadata, Player nextPlayer);
+};
+
 
 namespace NNInputs {
   const int NUM_FEATURES_SPATIAL_V3 = 22;
