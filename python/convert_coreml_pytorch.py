c10d6db6b94a6043bf5cc52e2178ace076014108
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-05-21 20:55:40 +0800

    Convert network to CoreML model with metadata encoder

--- python/convert_coreml_pytorch.py
+++ python/convert_coreml_pytorch.py
@@ -11,13 +11,13 @@ Convert a trained neural net to a CoreML model.
 """
 
 # Print torch version
-print(f'torch version: {torch.__version__}')
+print(f"torch version: {torch.__version__}")
 
 # Print coremltools version
-print(f'coremltools version: {ct.__version__}')
+print(f"coremltools version: {ct.__version__}")
 
 # Print coremlmish function
-print(f'Using coremlmish function: {coremlmish.__function__}')
+print(f"Using coremlmish function: {coremlmish.__function__}")
 
 
 def main():
@@ -25,24 +25,23 @@ def main():
     parser = argparse.ArgumentParser(description=description)
 
     # Add an argument of checkpoint file
-    parser.add_argument(
-        '-checkpoint', help='Checkpoint to test', required=True)
+    parser.add_argument("-checkpoint", help="Checkpoint to test", required=True)
 
     # Add an argument of use swa
-    parser.add_argument('-use-swa', help='Use SWA model',
-                        action="store_true", required=False)
+    parser.add_argument(
+        "-use-swa", help="Use SWA model", action="store_true", required=False
+    )
 
     # Add an argument of position length
-    parser.add_argument('-pos-len', help='Position length',
-                        type=int, required=False)
+    parser.add_argument("-pos-len", help="Position length", type=int, required=False)
 
     # Add an argument of batch size
-    parser.add_argument('-batch-size', help='Batch size',
-                        type=int, required=False)
+    parser.add_argument("-batch-size", help="Batch size", type=int, required=False)
 
     # Add an argument of 32-bit floating-point
-    parser.add_argument('-fp32', help='32-bit floating-point',
-                        action="store_true", required=False)
+    parser.add_argument(
+        "-fp32", help="32-bit floating-point", action="store_true", required=False
+    )
 
     # Parse the arguments
     args = vars(parser.parse_args())
@@ -54,33 +53,35 @@ def main():
     use_swa = args["use_swa"]
 
     # Get the argument of position length
-    pos_len = args['pos_len'] if args['pos_len'] else 19
+    pos_len = args["pos_len"] if args["pos_len"] else 19
 
     # Get the argument of batch size
-    batch_size = args['batch_size'] if args['batch_size'] else 1
+    batch_size = args["batch_size"] if args["batch_size"] else 1
 
     # Get the argument of 32-bit floating-point
-    fp32 = args['fp32']
+    fp32 = args["fp32"]
 
     # Load the model
     model, swa_model, _ = load_model(
         checkpoint_file,
-        use_swa, device="cpu",
+        use_swa,
+        device="cpu",
         pos_len=pos_len,
         for_coreml=True,
-        verbose=True)
+        verbose=True,
+    )
 
     # Set the model
     func = model if swa_model is None else swa_model
 
     # Print the model name
-    print(f'Using model: {func.__class__.__name__}')
+    print(f"Using model: {func.__class__.__name__}")
 
     # Get the model version
-    version = model.config['version']
+    version = model.config["version"]
 
     # Print the model version
-    print(f'Model version: {version}')
+    print(f"Model version: {version}")
 
     with torch.no_grad():
         # Set the model to eval mode
@@ -97,21 +98,48 @@ def main():
         # NC
         input_global = torch.rand(batch_size, model.global_input_shape[0])
 
+        # NC
+        input_meta = (
+            torch.rand(batch_size, model.metadata_encoder.c_input)
+            if model.metadata_encoder is not None
+            else None
+        )
+
+        # Set the example inputs
+        example_inputs = (
+            (input_spatial, input_global, input_meta)
+            if input_meta is not None
+            else (input_spatial, input_global)
+        )
+
         # Trace the model
-        print(f'Tracing model ...')
-        traced_model = torch.jit.trace(
-            func, (input_spatial, input_global))
+        print(f"Tracing model ...")
+        traced_model = torch.jit.trace(func, example_inputs)
 
         # Set the compute precision
         compute_precision = ct.precision.FLOAT16 if not fp32 else ct.precision.FLOAT32
 
+        # Set the input types
+        inputs = (
+            [
+                ct.TensorType(shape=input_spatial.shape),
+                ct.TensorType(shape=input_global.shape),
+                ct.TensorType(shape=input_meta.shape),
+            ]
+            if input_meta is not None
+            else [
+                ct.TensorType(shape=input_spatial.shape),
+                ct.TensorType(shape=input_global.shape),
+            ]
+        )
+
         # Convert the model
-        print(f'Converting model ...')
+        print(f"Converting model ...")
+
         mlmodel = ct.convert(
             traced_model,
             convert_to="mlprogram",
-            inputs=[ct.TensorType(shape=input_spatial.shape),
-                    ct.TensorType(shape=input_global.shape)],
+            inputs=inputs,
             compute_precision=compute_precision,
         )
 
@@ -119,53 +147,59 @@ def main():
         spec = mlmodel._spec
 
         # Rename the input
-        ct.utils.rename_feature(spec, 'input_1', 'input_global')
+        ct.utils.rename_feature(spec, "input_1", "input_global")
 
         # Get input names
         input_names = [input.name for input in spec.description.input]
 
         # Print the input names
-        print(f'Input names: {input_names}')
+        print(f"Input names: {input_names}")
 
         # Set output names
-        output_names = ['output_policy', 'out_value',
-                        'out_miscvalue', 'out_moremiscvalue', 'out_ownership']
+        output_names = [
+            "output_policy",
+            "out_value",
+            "out_miscvalue",
+            "out_moremiscvalue",
+            "out_ownership",
+        ]
 
         # Rename output names
         for i, name in enumerate(output_names):
             # Rename the output
-            ct.utils.rename_feature(
-                spec, spec.description.output[i].name, name)
+            ct.utils.rename_feature(spec, spec.description.output[i].name, name)
 
         # Print the output names
-        print(f'Output names: {output_names}')
+        print(f"Output names: {output_names}")
 
         # Set the compute precision name
-        precision_name = 'fp16' if not fp32 else 'fp32'
+        precision_name = "fp16" if not fp32 else "fp32"
 
         # Set file name
-        mlmodel_file = f'KataGoModel{pos_len}x{pos_len}{precision_name}' \
-            f'.mlpackage'
+        mlmodel_file = f"KataGoModel{pos_len}x{pos_len}{precision_name}" f".mlpackage"
 
         # Set model description
-        mlmodel.short_description = f'KataGo {pos_len}x{pos_len} compute ' \
-            f'precision {precision_name} model version {version} ' \
-            f'converted from {checkpoint_file}'
+        mlmodel.short_description = (
+            f"KataGo {pos_len}x{pos_len} compute "
+            f"precision {precision_name} model version {version} "
+            f"converted from {checkpoint_file}"
+        )
 
         # Set model version
-        mlmodel.version = f'{version}'
+        mlmodel.version = f"{version}"
 
         # Rebuild the model with the updated spec
-        print(f'Rebuilding model with updated spec ...')
+        print(f"Rebuilding model with updated spec ...")
         rebuilt_mlmodel = ct.models.MLModel(
-            mlmodel._spec, weights_dir=mlmodel._weights_dir)
+            mlmodel._spec, weights_dir=mlmodel._weights_dir
+        )
 
         # Save the model
-        print(f'Saving model ...')
+        print(f"Saving model ...")
         rebuilt_mlmodel.save(mlmodel_file)
 
         # Print the file name
-        print(f'Saved Core ML model at {mlmodel_file}')
+        print(f"Saved Core ML model at {mlmodel_file}")
 
 
 if __name__ == "__main__":
