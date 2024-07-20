8bfaf26ae2283a40117161bc68be74e2900e33f8
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-07-21 07:08:52 +0800

    Include human-trained model in KataGo setup script
    Added the new human-trained (meta) model files, processed downloads, and updated symbolic links.

--- cpp/xcode/setup.sh
+++ cpp/xcode/setup.sh
@@ -5,9 +5,13 @@ wget https://github.com/lightvector/KataGo/releases/download/v1.4.5/g170-b40c256
 mv g170-b40c256x2-s5095420928-d1229425124.bin.gz DerivedData/KataGo/Build/Products/Debug/modelv8.bin.gz
 wget https://github.com/ChinChangYang/KataGo/releases/download/v1.13.2-coreml1/KataGoModel19x19fp16v14s7709731328.mlpackage.zip
 mv KataGoModel19x19fp16v14s7709731328.mlpackage.zip DerivedData/KataGo/Build/Products/Debug/
-unzip DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage.zip -d DerivedData/KataGo/Build/Products/Debug/
 rm -rf DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16.mlpackage
+unzip DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage.zip -d DerivedData/KataGo/Build/Products/Debug/
 mv DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16v14s7709731328.mlpackage DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp16.mlpackage
+wget https://github.com/ChinChangYang/KataGo/releases/download/v1.15.1-coreml1/KataGoModel19x19fp32meta1.mlpackage.zip
+mv KataGoModel19x19fp32meta1.mlpackage.zip DerivedData/KataGo/Build/Products/Debug/
+rm -rf DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp32meta1.mlpackage
+unzip DerivedData/KataGo/Build/Products/Debug/KataGoModel19x19fp32meta1.mlpackage.zip -d DerivedData/KataGo/Build/Products/Debug/
 ln -s ../../../../../../configs/misc/coreml_example.cfg DerivedData/KataGo/Build/Products/Debug/gtp.cfg
 ln -s ../../../../../../configs/misc/metal_gtp.cfg DerivedData/KataGo/Build/Products/Debug/metal_gtp.cfg
 ln -s ../../../../../../tests DerivedData/KataGo/Build/Products/Debug/tests
