825305e944d8a3323b0ad81121c88d30a98e7078
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-07-28 19:47:45 +0800

    Update KataGo version from 1.15.1-coreml2 to 1.15.1-coreml3
    This commit updates the version number in the source code to reflect the new coreml3 version. Both the getKataGoVersion and getKataGoVersionForHelp methods have been modified to return the updated version string.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -210,11 +210,11 @@ int main(int argc, const char* const* argv) {
 
 
 string Version::getKataGoVersion() {
-  return string("1.15.1-coreml2");
+  return string("1.15.1-coreml3");
 }
 
 string Version::getKataGoVersionForHelp() {
-  return string("KataGo v1.15.1-coreml2");
+  return string("KataGo v1.15.1-coreml3");
 }
 
 string Version::getKataGoVersionFullInfo() {
