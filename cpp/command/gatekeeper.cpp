ddd6198f2fbd1a36ccd739457daf204aea366eb4
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-11-27 22:22:24 +0800

    Update gatekeeper to utilize Core ML model paths
    - Refactored gatekeeper to initialize neural network evaluators with Core ML model paths provided by the user.
    - Changed references from NNEvaluator to initializeCoreMLEvaluator for both test and accepted models.

--- cpp/command/gatekeeper.cpp
+++ cpp/command/gatekeeper.cpp
@@ -412,15 +412,15 @@ int MainCmds::gatekeeper(const vector<string>& args) {
     const bool disableFP16 = false;
     const string expectedSha256 = "";
 
-    NNEvaluator* testNNEval = Setup::initializeNNEvaluator(
-      testModelName,testModelFile,expectedSha256,cfg,logger,rand,expectedConcurrentEvals,
+    NNEvaluator* testNNEval = Setup::initializeCoreMLEvaluator(
+      testModelName,testModelFile,testModelDir,expectedSha256,cfg,logger,rand,expectedConcurrentEvals,
       maxBoardXSizeUsed,maxBoardYSizeUsed,defaultMaxBatchSize,defaultRequireExactNNLen,disableFP16,
       Setup::SETUP_FOR_OTHER
     );
     logger.write("Loaded candidate neural net " + testModelName + " from: " + testModelFile);
 
-    NNEvaluator* acceptedNNEval = Setup::initializeNNEvaluator(
-      acceptedModelName,acceptedModelFile,expectedSha256,cfg,logger,rand,expectedConcurrentEvals,
+    NNEvaluator* acceptedNNEval = Setup::initializeCoreMLEvaluator(
+      acceptedModelName,acceptedModelFile,acceptedModelDir,expectedSha256,cfg,logger,rand,expectedConcurrentEvals,
       maxBoardXSizeUsed,maxBoardYSizeUsed,defaultMaxBatchSize,defaultRequireExactNNLen,disableFP16,
       Setup::SETUP_FOR_OTHER
     );
