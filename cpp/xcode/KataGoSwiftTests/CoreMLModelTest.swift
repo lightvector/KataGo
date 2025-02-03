0ca48afc6ee2b8b029c0e5f7831b209adfc7ce55
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2025-02-03 12:55:35 +0800

    Remove CoreML model compilation tests from CoreMLModelTest.swift
    - Deleted tests that check for the existence of an old ML model, clean-up of URLs, and validation of model compilation.

--- cpp/xcode/KataGoSwiftTests/CoreMLModelTest.swift
+++ cpp/xcode/KataGoSwiftTests/CoreMLModelTest.swift
@@ -10,40 +10,6 @@ import XCTest
 final class CoreMLModelTest: XCTestCase {
     func testFreshCompileBundleMLModel() {
         let modelName = CoreMLBackend.getModelName()
-        let permanentURL = try! KataGoModel.getMLModelCPermanentURL(modelName: modelName)
-        let savedDigestURL = try! KataGoModel.getSavedDigestURL(modelName: modelName)
-        try! FileManager.default.removeItem(at: permanentURL)
-        try! FileManager.default.removeItem(at: savedDigestURL)
-
-        let mlmodel = KataGoModel.compileBundleMLModel(modelName: modelName,
-                                                       computeUnits: .cpuAndNeuralEngine)
-
-        XCTAssertNotNil(mlmodel)
-    }
-
-    func testCompileBundleMLModelWhenOldMLModelNotExists() {
-        let modelName = CoreMLBackend.getModelName()
-
-        _ = KataGoModel.compileBundleMLModel(modelName: modelName,
-                                             computeUnits: .cpuAndNeuralEngine)
-
-        let permanentURL = try! KataGoModel.getMLModelCPermanentURL(modelName: modelName)
-        try! FileManager.default.removeItem(at: permanentURL)
-
-        let mlmodel = KataGoModel.compileBundleMLModel(modelName: modelName,
-                                                       computeUnits: .cpuAndNeuralEngine)
-
-        XCTAssertNotNil(mlmodel)
-    }
-
-    func testCompileBundleMLModelWhenDigestChanges() {
-        let modelName = CoreMLBackend.getModelName()
-
-        _ = KataGoModel.compileBundleMLModel(modelName: modelName,
-                                             computeUnits: .cpuAndNeuralEngine)
-
-        let savedDigestURL = try! KataGoModel.getSavedDigestURL(modelName: modelName)
-        try! "".write(to: savedDigestURL, atomically: true, encoding: .utf8)
 
         let mlmodel = KataGoModel.compileBundleMLModel(modelName: modelName,
                                                        computeUnits: .cpuAndNeuralEngine)
